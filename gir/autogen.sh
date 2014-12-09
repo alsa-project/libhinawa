82c6db7cebc68aabd6f9c69a6a27d314930bd1c4
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2014-12-09 10:10:06 +0900

    remove old gir stuffs.

--- gir/autogen.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -u
-set -e
-
-mkdir -p m4
-
-gtkdocize --copy --docdir doc/reference
-autoreconf --install
--- gir/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-LD_LIBRARY_PATH=src/.libs/:/usr/lib:/lib GI_TYPELIB_PATH=src/:/usr/lib/girepository-1.0 python hinawa.py
