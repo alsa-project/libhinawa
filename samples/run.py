9f2e850cadced966bccbea2023dfb35038bc14fd
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2015-01-12 09:26:06 +0900

    samples: move to samples directory from tests directory

--- samples/run.py
+++ samples/run.py
@@ -7,9 +7,9 @@ export LD_LIBRARY_PATH=src/.libs/:/usr/lib:/lib
 export GI_TYPELIB_PATH=src/:/usr/lib/girepository-1.0
 
 if [[ $1 == 'qt4' ]] ; then
-	./tests/qt4.py
+	./samples/qt4.py
 elif [[ $1 == 'qt5' ]] ; then
-	./tests/qt5.py
+	./samples/qt5.py
 elif [[ $1 == 'gtk' ]] ; then
-	./tests/gtk3.py
+	./samples/gtk3.py
 fi
