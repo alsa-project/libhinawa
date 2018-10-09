alsa-project/libhinawa

commit 8205f2c40ab195c603ab80e3024ae1e67dd5a2e7

fw_unit/snd_unit: use blocking poll to save CPU usage

src/fw_unit.c
@@ -227,8 +227,8 @@ static void handle_update(HinawaFwUnit *self,
 
 static gboolean prepare_src(GSource *src, gint *timeout)
 {
-	/* Set 2msec for poll(2) timeout. */
-	*timeout = 2;
+	/* Use blocking poll(2) to save CPU usage. */
+	*timeout = -1;
 
 	/* This source is not ready, let's poll(2) */
 	return FALSE;

src/snd_unit.c
@@ -404,8 +404,8 @@ static void handle_lock_event(HinawaSndUnit *self,
 
 static gboolean prepare_src(GSource *src, gint *timeout)
 {
-	/* Set 2msec for poll(2) timeout. */
-	*timeout = 2;
+	/* Use blocking poll(2) to save CPU usage. */
+	*timeout = -1;
 
 	/* This source is not ready, let's poll(2) */
 	return FALSE;

commit b5f8cc627fe679cb31caff688971203b9a8f96d0

fw_unit/snd_unit: use 500 msec timeout for poll to block processing
This library executes poll(2) with blocking mode to save consumption of
CPU time, however there's no way to wake up from the blocking except
for emitting any UNIX signal. In a convention of design of library,
it's not good to emit signal to return from the blocking.

This commit expands timeout up to 500 msec. As a result, I/O thread
can return safely at cancellation.

Fixes: 8205f2c40ab1('fw_unit/snd_unit: use blocking poll to save CPU usage')

src/fw_unit.c
@@ -340,8 +340,8 @@ static void handle_update(HinawaFwUnit *self,
 
 static gboolean prepare_src(GSource *src, gint *timeout)
 {
-	/* Use blocking poll(2) to save CPU usage. */
-	*timeout = -1;
+	// Use 500 msec for safe cancellation of thread.
+	*timeout = 500;
 
 	/* This source is not ready, let's poll(2) */
 	return FALSE;

src/snd_unit.c
@@ -289,8 +289,8 @@ static void handle_lock_event(HinawaSndUnit *self,
 
 static gboolean prepare_src(GSource *src, gint *timeout)
 {
-	/* Use blocking poll(2) to save CPU usage. */
-	*timeout = -1;
+	// Use 500 msec for safe cancellation of thread.
+	*timeout = 500;
 
 	/* This source is not ready, let's poll(2) */
 	return FALSE;