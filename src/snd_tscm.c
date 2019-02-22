alsa-project/libhinawa

commit 613f64f6c8568cf752967d58b0d63c63b790385d

snd_tscm: add support for GObject signal of change of control for TASCAM FireWire series
ALSA firewire-tascam driver supports event notification for change of
some control surface.

This commit adds support for the notification. The events are handled
with enough care of endianness and emitted as a GObject signal.

src/hinawa.map
@@ -77,4 +77,5 @@ HINAWA_1_1_0 {
     "hinawa_snd_tscm_get_type";
     "hinawa_snd_tscm_open";
     "hinawa_snd_tscm_quark";
+    "hinawa_sigs_marshal_VOID__UINT_UINT";
 } HINAWA_1_0_0;

src/snd_tscm.c
@@ -19,9 +19,32 @@ G_DEFINE_QUARK("HinawaSndTscm", hinawa_snd_tscm)
 
 G_DEFINE_TYPE(HinawaSndTscm, hinawa_snd_tscm, HINAWA_TYPE_SND_UNIT)
 
+/* This object has one signal. */
+enum tscm_sig_type {
+        TSCM_SIG_TYPE_CTL,
+        TSCM_SIG_TYPE_COUNT,
+};
+static guint tscm_sigs[TSCM_SIG_TYPE_COUNT] = { 0 };
+
 static void hinawa_snd_tscm_class_init(HinawaSndTscmClass *klass)
 {
-	return;
+	/**
+	 * HinawaSndTscm::control:
+	 * @self: A #HinawaSndTscm
+	 * @flags: A bitmask of model-dependent control
+	 *
+	 * When TASCAM FireWire unit transfer control message, the ::control
+	 * signal is emitted.
+	 */
+	tscm_sigs[TSCM_SIG_TYPE_CTL] =
+		g_signal_new("control",
+			     G_OBJECT_CLASS_TYPE(klass),
+			     G_SIGNAL_RUN_LAST,
+			     0,
+			     NULL, NULL,
+			     hinawa_sigs_marshal_VOID__UINT_UINT_UINT,
+			     G_TYPE_NONE,
+			     3, G_TYPE_UINT, G_TYPE_UINT, G_TYPE_UINT);
 }
 
 static void hinawa_snd_tscm_init(HinawaSndTscm *self)
@@ -51,5 +74,29 @@ void hinawa_snd_tscm_open(HinawaSndTscm *self, gchar *path, GError **exception)
 	if (type != SNDRV_FIREWIRE_TYPE_TASCAM) {
 		raise(exception, EINVAL);
 		return;
+	}
+}
+
+void hinawa_snd_tscm_handle_control(HinawaSndTscm *self, const void *buf,
+				    unsigned int len)
+{
+	struct snd_firewire_event_tascam_control *event =
+			(struct snd_firewire_event_tascam_control *)buf;
+	struct snd_firewire_tascam_change *change;
+
+	g_return_if_fail(HINAWA_IS_SND_TSCM(self));
+
+	if (len < sizeof(event->type))
+		return;
+	len -= sizeof(event->type);
+
+	change = event->changes;
+	while (len >= sizeof(*change)) {
+		g_signal_emit(self, tscm_sigs[TSCM_SIG_TYPE_CTL], 0,
+			      change->index,
+			      GUINT32_FROM_BE(change->before),
+			      GUINT32_FROM_BE(change->after));
+		++change;
+		len -= sizeof(*change);
 	}
 }

src/hinawa_sigs_marshal.list
@@ -1,1 +1,2 @@
 ENUM:ENUM
+VOID:UINT,UINT,UINT

src/internal.h
@@ -21,6 +21,7 @@
 #include "snd_efw.h"
 #include "snd_dg00x.h"
 #include "snd_motu.h"
+#include "snd_tscm.h"
 
 void hinawa_fw_unit_ioctl(HinawaFwUnit *self, unsigned long req, void *args,
 			  int *err);
@@ -48,6 +49,10 @@ void hinawa_snd_dg00x_handle_msg(HinawaSndDg00x *self, const void *buf,
 #if HAVE_SND_MOTU
 void hinawa_snd_motu_handle_notification(HinawaSndMotu *self,
 					 const void *buf, unsigned int len);
+#endif
+#if HAVE_SND_TSCM
+void hinawa_snd_tscm_handle_control(HinawaSndTscm *self, const void *buf,
+				    unsigned int len);
 #endif
 
 #endif

src/snd_unit.c
@@ -358,6 +358,12 @@ static gboolean check_src(GSource *gsrc)
 		hinawa_snd_motu_handle_notification(HINAWA_SND_MOTU(unit),
 						    priv->buf, len);
 #endif
+#if HAVE_SND_TSCM
+	else if (HINAWA_IS_SND_TSCM(unit) &&
+		 common->type == SNDRV_FIREWIRE_EVENT_TASCAM_CONTROL)
+		hinawa_snd_tscm_handle_control(HINAWA_SND_TSCM(unit),
+						    priv->buf, len);
+#endif
 end:
 	/* Don't go to dispatch, then continue to process this source. */
 	return FALSE;

commit 7e114f227562877627a8b7571d43f614a1574951

snd_tscm: fix signature of exported symbol for 'control' GObject signal
The committed signature is not correct.

Fixes: 613f64f6c856 ("snd_tscm: add support for GObject signal of change of control for TASCAM FireWire series")

src/hinawa.map
@@ -77,6 +77,6 @@ HINAWA_1_1_0 {
     "hinawa_snd_tscm_get_type";
     "hinawa_snd_tscm_open";
     "hinawa_snd_tscm_quark";
-    "hinawa_sigs_marshal_VOID__UINT_UINT";
+    "hinawa_sigs_marshal_VOID__UINT_UINT_UINT";
     "hinawa_snd_tscm_get_state";
 } HINAWA_1_0_0;

src/snd_tscm.c
@@ -3,7 +3,6 @@
 
 #include "internal.h"
 #include "snd_tscm.h"
-#include "hinawa_sigs_marshal.h"
 
 /**
  * SECTION:snd_tscm

src/snd_tscm.h
@@ -4,6 +4,7 @@
 #include <glib.h>
 #include <glib-object.h>
 #include "snd_unit.h"
+#include "hinawa_sigs_marshal.h"
 
 G_BEGIN_DECLS
 