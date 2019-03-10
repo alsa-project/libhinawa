alsa-project/libhinawa

commit 4fb5ff566d90a0a04b0759e00ace75563752ab9b

snd_tscm: add a method to retrieve current state of unit
ALSA firewire-tascam driver allows userspace applications to retrieve
the latest states of control surface.

This commit adds support for the feature with enough care of endianness.

src/snd_tscm.c
@@ -17,7 +17,11 @@ G_DEFINE_QUARK("HinawaSndTscm", hinawa_snd_tscm)
 	g_set_error(exception, hinawa_snd_tscm_quark(), errno,	\
 		    "%d: %s", __LINE__, strerror(errno))
 
-G_DEFINE_TYPE(HinawaSndTscm, hinawa_snd_tscm, HINAWA_TYPE_SND_UNIT)
+struct _HinawaSndTscmPrivate {
+	struct snd_firewire_tascam_state image;
+	guint32 state[SNDRV_FIREWIRE_TASCAM_STATE_COUNT];
+};
+G_DEFINE_TYPE_WITH_PRIVATE(HinawaSndTscm, hinawa_snd_tscm, HINAWA_TYPE_SND_UNIT)
 
 /* This object has one signal. */
 enum tscm_sig_type {
@@ -75,6 +79,32 @@ void hinawa_snd_tscm_open(HinawaSndTscm *self, gchar *path, GError **exception)
 		raise(exception, EINVAL);
 		return;
 	}
+}
+
+/**
+ * hinawa_snd_tscm_get_state:
+ * @self: A #HinawaSndTscm
+ *
+ * Returns: (element-type guint32) (array fixed-size=64) (transfer none): state
+ * 	    image.
+ *
+ */
+const guint32 *const hinawa_snd_tscm_get_state(HinawaSndTscm *self,
+					       GError **exception)
+{
+	HinawaSndTscmPrivate *priv;
+	int i;
+
+	g_return_val_if_fail(HINAWA_IS_SND_TSCM(self), NULL);
+	priv = hinawa_snd_tscm_get_instance_private(self);
+
+	hinawa_snd_unit_ioctl(&self->parent_instance,
+			      SNDRV_FIREWIRE_IOCTL_TASCAM_STATE, &priv->image,
+			      exception);
+
+	for (i = 0; i < SNDRV_FIREWIRE_TASCAM_STATE_COUNT; ++i)
+		priv->state[i] = GUINT32_FROM_BE(priv->image.data[i]);
+	return priv->state;
 }
 
 void hinawa_snd_tscm_handle_control(HinawaSndTscm *self, const void *buf,

src/snd_tscm.h
@@ -31,9 +31,12 @@ G_BEGIN_DECLS
 
 typedef struct _HinawaSndTscm		HinawaSndTscm;
 typedef struct _HinawaSndTscmClass	HinawaSndTscmClass;
+typedef struct _HinawaSndTscmPrivate	HinawaSndTscmPrivate;
 
 struct _HinawaSndTscm {
 	HinawaSndUnit parent_instance;
+
+	HinawaSndTscmPrivate *priv;
 };
 
 struct _HinawaSndTscmClass {
@@ -43,5 +46,8 @@ struct _HinawaSndTscmClass {
 GType hinawa_snd_tscm_get_type(void) G_GNUC_CONST;
 
 void hinawa_snd_tscm_open(HinawaSndTscm *self, gchar *path, GError **exception);
+
+const guint32 *const hinawa_snd_tscm_get_state(HinawaSndTscm *self,
+					       GError **exception);
 
 #endif

src/hinawa.map
@@ -78,4 +78,5 @@ HINAWA_1_1_0 {
     "hinawa_snd_tscm_open";
     "hinawa_snd_tscm_quark";
     "hinawa_sigs_marshal_VOID__UINT_UINT";
+    "hinawa_snd_tscm_get_state";
 } HINAWA_1_0_0;

src/internal.h
@@ -34,6 +34,9 @@ void hinawa_snd_unit_write(HinawaSndUnit *self,
 			   const void *buf, unsigned int length,
 			   GError **exception);
 
+void hinawa_snd_unit_ioctl(HinawaSndUnit *self, unsigned long request,
+			   void *arg, GError **exception);
+
 #if HAVE_SND_DICE
 void hinawa_snd_dice_handle_notification(HinawaSndDice *self,
 					 const void *buf, unsigned int len);

src/snd_unit.c
@@ -275,6 +275,18 @@ void hinawa_snd_unit_write(HinawaSndUnit *self,
 		raise(exception, errno);
 }
 
+void hinawa_snd_unit_ioctl(HinawaSndUnit *self, unsigned long request,
+			   void *arg, GError **exception)
+{
+	HinawaSndUnitPrivate *priv;
+
+	g_return_if_fail(HINAWA_IS_SND_UNIT(self));
+	priv = hinawa_snd_unit_get_instance_private(self);
+
+	if (ioctl(priv->fd, request, arg) < 0)
+		raise(exception, errno);
+}
+
 static void handle_lock_event(HinawaSndUnit *self,
 			      void *buf, unsigned int length)
 {

commit 3ce5e3394752e04d667845b4c9519235d7c86d94

snd_tscm: fix signature of 'get_state'
The returned pointer is not mutable.

Fixes: 4fb5ff566d90a0a0: ("snd_tscm: add a method to retrieve current state of unit")

src/snd_tscm.c
@@ -96,8 +96,8 @@ void hinawa_snd_tscm_open(HinawaSndTscm *self, gchar *path, GError **exception)
  * 	    image.
  *
  */
-const guint32 *const hinawa_snd_tscm_get_state(HinawaSndTscm *self,
-					       GError **exception)
+const guint32 *hinawa_snd_tscm_get_state(HinawaSndTscm *self,
+					 GError **exception)
 {
 	HinawaSndTscmPrivate *priv;
 	int i;

src/snd_tscm.h
@@ -61,7 +61,7 @@ GType hinawa_snd_tscm_get_type(void) G_GNUC_CONST;
 
 void hinawa_snd_tscm_open(HinawaSndTscm *self, gchar *path, GError **exception);
 
-const guint32 *const hinawa_snd_tscm_get_state(HinawaSndTscm *self,
-					       GError **exception);
+const guint32 *hinawa_snd_tscm_get_state(HinawaSndTscm *self,
+					 GError **exception);
 
 #endif