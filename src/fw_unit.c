d9f0207b8a3bed93315d457385966b9f6c54e3a5
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2019-10-11 19:25:23 +0900

    fw_unit: update documentation with deprecated hints

--- src/fw_unit.c
+++ src/fw_unit.c
@@ -9,7 +9,7 @@
 
 #include "internal.h"
 
-/* For error handling. */
+// For error handling.
 G_DEFINE_QUARK("HinawaFwUnit", hinawa_fw_unit)
 #define raise(exception, errno)						\
 	g_set_error(exception, hinawa_fw_unit_quark(), errno,		\
@@ -18,11 +18,17 @@ G_DEFINE_QUARK("HinawaFwUnit", hinawa_fw_unit)
 /**
  * SECTION:fw_unit
  * @Title: HinawaFwUnit
- * @Short_description: An event listener for FireWire unit
+ * @Short_description: An object to maintain association between IEEE 1394 unit
+ *		       and its parent.
  *
- * A #HinawaFwUnit is an event listener for a certain FireWire unit.
- * This class is an application of Linux FireWire subsystem.
- * All of operations utilize ioctl(2) with subsystem specific request commands.
+ * A #HinawaFwUnit object has a reference to an instance of HinawaFwNode for
+ * Linux FireWire character device, corresponding to parent node on IEEE 1394
+ * bus. This object is expected to be used by inheritance from subclasses; e.g.
+ * HinawaSndUnit.
+ *
+ * All of operations are done by associated HinawaFwNode. Some APIs and
+ * properties which #HinawaFwUnit has are maintained just for backward
+ * compatibility and already deprecated. Instead, use associated HinawaFwNode.
  */
 
 struct _HinawaFwUnitPrivate {
@@ -39,7 +45,7 @@ typedef struct {
 	void *buf;
 } FwUnitSource;
 
-/* This object has properties. */
+// This object has deprecated properties.
 enum fw_unit_prop_type {
 	FW_UNIT_PROP_TYPE_NODE_ID = 1,
 	FW_UNIT_PROP_TYPE_LOCAL_NODE_ID,
@@ -52,7 +58,7 @@ enum fw_unit_prop_type {
 };
 static GParamSpec *fw_unit_props[FW_UNIT_PROP_TYPE_COUNT] = { NULL, };
 
-/* This object has one signal. */
+// This object has two deprecated signals.
 enum fw_unit_sig_type {
 	FW_UNIT_SIG_TYPE_BUS_UPDATE = 0,
 	FW_UNIT_SIG_TYPE_DISCONNECTED,
@@ -115,42 +121,42 @@ static void hinawa_fw_unit_class_init(HinawaFwUnitClass *klass)
 		g_param_spec_ulong("node-id", "node-id",
 				   "Node-ID of this unit at this generation.",
 				   0, ULONG_MAX, 0,
-				   G_PARAM_READABLE);
+				   G_PARAM_READABLE | G_PARAM_DEPRECATED);
 	fw_unit_props[FW_UNIT_PROP_TYPE_LOCAL_NODE_ID] =
 		g_param_spec_ulong("local-node-id", "local-node-id",
 				   "Node-ID for a unit which this unit use to "
 				   "communicate to the other units on the bus "
 				   "at this generation.",
 				   0, ULONG_MAX, 0,
-				   G_PARAM_READABLE);
+				   G_PARAM_READABLE | G_PARAM_DEPRECATED);
 	fw_unit_props[FW_UNIT_PROP_TYPE_BUS_MANAGER_NODE_ID] =
 		g_param_spec_ulong("bus-manager-node-id", "bus-manager-node-id",
 				   "Node-ID for bus manager on the bus at this "
 				   "generation.",
 				   0, ULONG_MAX, 0,
-				   G_PARAM_READABLE);
+				   G_PARAM_READABLE | G_PARAM_DEPRECATED);
 	fw_unit_props[FW_UNIT_PROP_TYPE_IR_MANAGER_NODE_ID] =
 		g_param_spec_ulong("ir-manager-node-id", "ir-manager-node-id",
 				   "Node-ID for isochronous resource manager "
 				   "on the bus at this generation",
 				   0, ULONG_MAX, 0,
-				   G_PARAM_READABLE);
+				   G_PARAM_READABLE | G_PARAM_DEPRECATED);
 	fw_unit_props[FW_UNIT_PROP_TYPE_ROOT_NODE_ID] =
 		g_param_spec_ulong("root-node-id", "root-node-id",
 				   "Node-ID for root of bus topology at this "
 				   "generation.",
 				   0, ULONG_MAX, 0,
-				   G_PARAM_READABLE);
+				   G_PARAM_READABLE | G_PARAM_DEPRECATED);
 	fw_unit_props[FW_UNIT_PROP_TYPE_GENERATION] =
 		g_param_spec_ulong("generation", "generation",
 				   "current level of generation on this bus.",
 				   0, ULONG_MAX, 0,
-				   G_PARAM_READABLE);
+				   G_PARAM_READABLE | G_PARAM_DEPRECATED);
 	fw_unit_props[FW_UNIT_PROP_TYPE_LISTENING] =
 		g_param_spec_boolean("listening", "listening",
 				     "Whether this device is under listening.",
 				     FALSE,
-				     G_PARAM_READABLE);
+				     G_PARAM_READABLE | G_PARAM_DEPRECATED);
 
 	g_object_class_install_properties(gobject_class,
 					  FW_UNIT_PROP_TYPE_COUNT,
@@ -163,6 +169,9 @@ static void hinawa_fw_unit_class_init(HinawaFwUnitClass *klass)
 	 * When IEEE 1394 bus is updated, the ::bus-update signal is generated.
 	 * Handlers can read current generation in the bus via 'generation'
 	 * property.
+	 *
+	 * Deprecated: 1.4.0: Use an instance of HinawaFwNode retrieved by a
+	 *		      call of hinawa_fw_unit_get_node().
 	 */
 	fw_unit_sigs[FW_UNIT_SIG_TYPE_BUS_UPDATE] =
 		g_signal_new("bus-update",
@@ -177,8 +186,11 @@ static void hinawa_fw_unit_class_init(HinawaFwUnitClass *klass)
 	 * HinawaFwUnit::disconnected:
 	 * @self: A #HinawaFwUnit
 	 *
-	 * When phicical FireWire devices are disconnected from IEEE 1394 bus,
-	 * the #HinawaFwUnit becomes unlostening and emits this signal.
+	 * When physical FireWire devices are disconnected from IEEE 1394 bus,
+	 * the #HinawaFwUnit becomes unlistening and emits this signal.
+	 *
+	 * Deprecated: 1.4.0: Use an instance of HinawaFwNode retrieved by a
+	 *		      call of hinawa_fw_unit_get_node().
 	 */
 	fw_unit_sigs[FW_UNIT_SIG_TYPE_DISCONNECTED] =
 		g_signal_new("disconnected",
@@ -251,11 +263,16 @@ void hinawa_fw_unit_open(HinawaFwUnit *self, gchar *path, GError **exception)
 /**
  * hinawa_fw_unit_get_config_rom:
  * @self: A #HinawaFwUnit
- * @length: (out) (optional): the number of bytes consists of the config rom
+ * @length: (out)(optional): the number of bytes consists of the config rom
  *
  * Get cached content of configuration ROM.
  *
- * Returns: (element-type guint8) (array length=length) (transfer none): config rom image
+ * Returns: (element-type guint8)(array length=length)(transfer none): config
+ *	    rom image.
+ *
+ * Deprecated: 1.4.0: Instead, use hinawa_fw_node_get_config_rom() for an
+ *		      instance of HinawaFwNode retrieved by a call of
+ *		      hinawa_fw_unit_get_node().
  */
 const guint8 *hinawa_fw_unit_get_config_rom(HinawaFwUnit *self, guint *length)
 {
@@ -280,6 +297,12 @@ const guint8 *hinawa_fw_unit_get_config_rom(HinawaFwUnit *self, guint *length)
  * @exception: A #GError
  *
  * Start to listen to any events from the unit.
+ *
+ * Deprecated: 1.4.0: Instead, use GSource retrieved by a call of
+ *		      hinawa_fw_node_create_source() for an instance of
+ *		      HinawaFwNode retrieved by a call of
+ *		      hinawa_fw_unit_get_node(). Then use GMainContext and
+ *		      GMainLoop of GLib for event loop.
  */
 void hinawa_fw_unit_listen(HinawaFwUnit *self, GError **exception)
 {
@@ -308,6 +331,9 @@ void hinawa_fw_unit_listen(HinawaFwUnit *self, GError **exception)
  * @self: A #HinawaFwUnit
  *
  * Stop to listen to any events from the unit.
+ *
+ * Deprecated: 1.4.0: Instead, maintain GMainContext and GMainLoop with GSource
+ *		      retrieved by a call of hinawa_fw_node_create_source().
  */
 void hinawa_fw_unit_unlisten(HinawaFwUnit *self)
 {
