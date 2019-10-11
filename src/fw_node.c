b00ac9953d68284f34554d30edb735a12e0c24f1
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2019-10-11 19:25:23 +0900

    fw_node: add an GObject object to represent node on IEEE 1394 bus

---
+++ src/fw_node.c
@@ -0,0 +1,39 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+#include "fw_node.h"
+
+/**
+ * SECTION:fw_node
+ * @Title: HinawaFwNode
+ * @Short_description: An event listener for FireWire node
+ *
+ * A #HinawaFwNode is an event listener for a specified node on IEEE 1394 bus.
+ * This class is an application of Linux FireWire subsystem.
+ * All of operations utilize ioctl(2) with subsystem specific request commands.
+ *
+ * Since: 1.4
+ */
+
+G_DEFINE_TYPE(HinawaFwNode, hinawa_fw_node, G_TYPE_OBJECT)
+
+static void hinawa_fw_node_class_init(HinawaFwNodeClass *klass)
+{
+	return;
+}
+
+static void hinawa_fw_node_init(HinawaFwNode *self)
+{
+	return;
+}
+
+/**
+ * hinawa_fw_node_new:
+ *
+ * Instantiate #HinawaFwNode object and return the instance.
+ *
+ * Returns: an instance of #HinawaFwNode.
+ * Since: 1.4.
+ */
+HinawaFwNode *hinawa_fw_node_new(void)
+{
+	return g_object_new(HINAWA_TYPE_FW_NODE, NULL);
+}
---
+++ src/fw_node.h
@@ -0,0 +1,49 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+#ifndef __ALSA_TOOLS_HINAWA_FW_NODE_H__
+#define __ALSA_TOOLS_HINAWA_FW_NODE_H__
+
+#include <glib.h>
+#include <glib-object.h>
+
+G_BEGIN_DECLS
+
+#define HINAWA_TYPE_FW_NODE	(hinawa_fw_node_get_type())
+
+#define HINAWA_FW_NODE(obj)					\
+	(G_TYPE_CHECK_INSTANCE_CAST((obj),			\
+				    HINAWA_TYPE_FW_NODE,	\
+				    HinawaFwNode))
+#define HINAWA_IS_FW_NODE(obj)					\
+	(G_TYPE_CHECK_INSTANCE_TYPE((obj),			\
+				    HINAWA_TYPE_FW_NODE))
+
+#define HINAWA_FW_NODE_CLASS(klass)				\
+	(G_TYPE_CHECK_CLASS_CAST((klass),			\
+				 HINAWA_TYPE_FW_NODE,		\
+				 HinawaFwNodeClass))
+#define HINAWA_IS_FW_NODE_CLASS(klass)				\
+	(G_TYPE_CHECK_CLASS_TYPE((klass),			\
+				 HINAWA_TYPE_FW_NODE))
+#define HINAWA_FW_NODE_GET_CLASS(obj)				\
+	(G_TYPE_INSTANCE_GET_CLASS((obj),			\
+				   HINAWA_TYPE_FW_NODE,		\
+				   HinawaFwNodeClass))
+
+typedef struct _HinawaFwNode		HinawaFwNode;
+typedef struct _HinawaFwNodeClass	HinawaFwNodeClass;
+
+struct _HinawaFwNode {
+	GObject parent_instance;
+};
+
+struct _HinawaFwNodeClass {
+	GObjectClass parent_class;
+};
+
+GType hinawa_fw_node_get_type(void) G_GNUC_CONST;
+
+HinawaFwNode *hinawa_fw_node_new(void);
+
+G_END_DECLS
+
+#endif
