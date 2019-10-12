5bd270fcd453b4fa39163845951891e12ad2a53d
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2019-10-12 11:49:46 +0900

    samples: launch single thread to dispatch event

--- samples/gtk3.py
+++ samples/gtk3.py
@@ -48,14 +48,18 @@ if 'unit' not in locals():
     print('No sound FireWire devices found.')
     exit()
 
-# Start a thread to dispatch events from the sound unit.
-unit_ctx = GLib.MainContext.new()
+# Start a thread to dispatch events from the sound unit and node.
+ctx = GLib.MainContext.new()
 unit_src = unit.create_source()
-unit_src.attach(unit_ctx)
-unit_dispatcher = GLib.MainLoop.new(unit_ctx, False)
-unit_th = Thread(target=lambda d: d.run(), args=(unit_dispatcher,))
-unit_th.start()
+unit_src.attach(ctx)
+node = unit.get_node()
+node_src = node.create_source()
+node_src.attach(ctx)
+dispatcher = GLib.MainLoop.new(ctx, False)
+dispatch_th = Thread(target=lambda d: d.run(), args=(dispatcher,))
+dispatch_th.start()
 
+# Setup sound unit.
 def handle_lock_status(unit, status):
     if status:
         print("streaming is locked.");
@@ -72,15 +76,7 @@ print(' card:\t\t{0}'.format(unit.get_property("card")))
 print(' device:\t{0}'.format(unit.get_property("device")))
 print(' GUID:\t\t{0:016x}'.format(unit.get_property("guid")))
 
-# Start a thread to dispatch events from the node.
-node = unit.get_node()
-node_ctx = GLib.MainContext.new()
-node_src = node.create_source()
-node_src.attach(node_ctx)
-node_dispatcher = GLib.MainLoop.new(node_ctx, False)
-node_th = Thread(target=lambda d: d.run(), args=(node_dispatcher,))
-node_th.start()
-
+# Setup firewire node.
 def handle_bus_update(node):
     print('bus-reset: generation {0}'.format(node.get_property('generation')))
 node.connect("bus-update", handle_bus_update)
@@ -247,15 +243,11 @@ Gtk.main()
 del win
 print('delete window object')
 
-unit_dispatcher.quit()
-unit_th.join()
+dispatcher.quit()
+dispatch_th.join()
 del unit_src
-del unit_dispatcher
-
-node_dispatcher.quit()
-node_th.join()
 del node_src
-del node_dispatcher
+del dispatcher
 
 resp.release()
 del resp
