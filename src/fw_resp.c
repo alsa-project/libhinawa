alsa-project/libhinawa

commit 5306410ddaea24859f848ddf38f44461557821d5

fw_unit: add ioctl method for internal use
And purge 'fd' property.

src/fw_resp.c
@@ -1,5 +1,6 @@
 #include <sys/ioctl.h>
 #include "fw_resp.h"
+#include "internal.h"
 #include "hinawa_sigs_marshal.h"
 
 #ifdef HAVE_CONFIG_H
@@ -7,7 +8,8 @@
 #endif
 
 struct _HinawaFwRespPrivate {
-	gint fd;
+	HinawaFwUnit *unit;
+
 	guchar *buf;
 	guint width;
 	guint64 addr_handle;
@@ -30,8 +32,11 @@ static guint fw_resp_sigs[FW_RESP_SIG_TYPE_COUNT] = { 0 };
 static void fw_resp_dispose(GObject *gobject)
 {
 	HinawaFwResp *self = HINAWA_FW_RESP(gobject);
+	HinawaFwRespPrivate *priv = FW_RESP_GET_PRIVATE(self);
 
 	hinawa_fw_resp_unregister(self);
+	g_object_unref(priv->unit);
+
 	G_OBJECT_CLASS(hinawa_fw_resp_parent_class)->dispose(gobject);
 }
 
@@ -77,6 +82,7 @@ static void hinawa_fw_resp_init(HinawaFwResp *self)
 HinawaFwResp *hinawa_fw_resp_new(HinawaFwUnit *unit, GError **exception)
 {
 	HinawaFwResp *self;
+	HinawaFwRespPrivate *priv;
 
 	self = g_object_new(HINAWA_TYPE_FW_RESP, NULL);
 	if (self == NULL) {
@@ -84,8 +90,10 @@ HinawaFwResp *hinawa_fw_resp_new(HinawaFwUnit *unit, GError **exception)
 			    ENOMEM, "%s", strerror(ENOMEM));
 		return NULL;
 	}
+	priv = FW_RESP_GET_PRIVATE(self);
 
-	g_object_get(G_OBJECT(unit), "fd", &self->priv->fd, NULL);
+	g_object_ref(unit);
+	priv->unit = unit;
 
 	return self;
 }
@@ -95,6 +103,7 @@ void hinawa_fw_resp_register(HinawaFwResp *self, guint64 addr, guint width,
 {
 	HinawaFwRespPrivate *priv;
 	struct fw_cdev_allocate allocate = {0};
+	int err;
 
 	g_return_if_fail(HINAWA_IS_FW_RESP(self));
 	priv = FW_RESP_GET_PRIVATE(self);
@@ -104,9 +113,10 @@ void hinawa_fw_resp_register(HinawaFwResp *self, guint64 addr, guint width,
 	allocate.length = width;
 	allocate.region_end = addr + width;
 
-	if (ioctl(self->priv->fd, FW_CDEV_IOC_ALLOCATE, &allocate) < 0) {
+	hinawa_fw_unit_ioctl(priv->unit, FW_CDEV_IOC_ALLOCATE, &allocate, &err);
+	if (err != 0) {
 		g_set_error(exception, g_quark_from_static_string(__func__),
-			    errno, "%s", strerror(errno));
+			    err, "%s", strerror(err));
 		return;
 	}
 
@@ -124,12 +134,16 @@ void hinawa_fw_resp_register(HinawaFwResp *self, guint64 addr, guint width,
 
 void hinawa_fw_resp_unregister(HinawaFwResp *self)
 {
+	HinawaFwRespPrivate *priv;
 	struct fw_cdev_deallocate deallocate = {0};
+	int err;
 
 	g_return_if_fail(HINAWA_IS_FW_RESP(self));
+	priv = FW_RESP_GET_PRIVATE(self);
 
-	deallocate.handle = self->priv->addr_handle;
-	ioctl(self->priv->fd, FW_CDEV_IOC_DEALLOCATE, &deallocate);
+	deallocate.handle = priv->addr_handle;
+	hinawa_fw_unit_ioctl(priv->unit, FW_CDEV_IOC_DEALLOCATE, &deallocate,
+			     &err);
 }
 
 /**
@@ -176,6 +190,7 @@ void hinawa_fw_resp_handle_request(int fd, union fw_cdev_event *ev)
 
 	struct fw_cdev_send_response resp = {0};
 	gboolean error;
+	int err;
 
 	if (event->length > priv->width) {
 		resp.rcode = RCODE_CONFLICT_ERROR;
@@ -209,7 +224,7 @@ void hinawa_fw_resp_handle_request(int fd, union fw_cdev_event *ev)
 respond:
 	resp.handle = event->handle;
 
-	ioctl(priv->fd, FW_CDEV_IOC_SEND_RESPONSE, &resp);
+	hinawa_fw_unit_ioctl(priv->unit, FW_CDEV_IOC_SEND_RESPONSE, &resp, &err);
 
 	priv->len = 0;
 	if (frame == NULL)

src/fw_req.c
@@ -13,7 +13,6 @@ enum fw_req_type {
 
 /* NOTE: This object has no properties and no signals. */
 struct _HinawaFwReqPrivate {
-	gint fd;
 	GArray *frame;
 
 	GCond cond;
@@ -68,7 +67,6 @@ static void fw_req_transact(HinawaFwReq *self, HinawaFwUnit *unit,
 	HinawaFwReqPrivate *priv = FW_REQ_GET_PRIVATE(self);
 	int tcode;
 
-	int fd;
 	guint64 generation;
 
 	guint64 expiration;
@@ -110,7 +108,6 @@ static void fw_req_transact(HinawaFwReq *self, HinawaFwUnit *unit,
 	}
 
 	/* Get unit properties. */
-	g_object_get(G_OBJECT(unit), "fd", &fd, NULL);
 	g_object_get(G_OBJECT(unit), "generation", &generation, NULL);
 
 	/* Setup a transaction structure. */
@@ -126,10 +123,9 @@ static void fw_req_transact(HinawaFwReq *self, HinawaFwUnit *unit,
 	g_mutex_init(&lock);
 
 	/* Send this transaction. */
-	if (ioctl(fd, FW_CDEV_IOC_SEND_REQUEST, &req) < 0) {
-		*err = errno;
+	hinawa_fw_unit_ioctl(unit, FW_CDEV_IOC_SEND_REQUEST, &req, err);
 	/* Wait for a response with timeout, waken by a response handler. */
-	} else {
+	if (*err == 0) {
 		g_mutex_lock(&lock);
 		if (!g_cond_wait_until(&priv->cond, &lock, expiration))
 			*err = ETIMEDOUT;
@@ -250,14 +246,14 @@ void hinawa_fw_req_handle_response(int fd, union fw_cdev_event *ev)
 	priv = FW_REQ_GET_PRIVATE(req);
 
 	/* Copy transaction frame. */
-	if (req->priv->frame != NULL) {
+	if (priv->frame != NULL) {
 		priv->frame->len = 0;
 		g_array_append_vals(priv->frame, event->data,
 			event->length / g_array_get_element_size(priv->frame));
 	}
 
 	/* Waken a thread of an user application. */
-	g_cond_signal(&req->priv->cond);
+	g_cond_signal(&priv->cond);
 
 	return;
 }

src/fw_req.h
@@ -3,8 +3,8 @@
 
 #include <glib.h>
 #include <glib-object.h>
-#include "internal.h"
 #include "fw_unit.h"
+#include "internal.h"
 
 G_BEGIN_DECLS
 

src/fw_unit.c
@@ -6,6 +6,7 @@
 
 #include "hinawa_context.h"
 #include "fw_unit.h"
+#include "internal.h"
 
 #ifdef HAVE_CONFIG_H
 #  include <config.h>
@@ -32,8 +33,7 @@ G_DEFINE_TYPE_WITH_PRIVATE(HinawaFwUnit, hinawa_fw_unit, G_TYPE_OBJECT)
 
 /* This object has properties. */
 enum fw_unit_prop_type {
-	FW_UNIT_PROP_TYPE_FD = 1,
-	FW_UNIT_PROP_TYPE_GENERATION,
+	FW_UNIT_PROP_TYPE_GENERATION = 1,
 	FW_UNIT_PROP_TYPE_COUNT,
 };
 static GParamSpec *fw_unit_props[FW_UNIT_PROP_TYPE_COUNT] = { NULL, };
@@ -52,9 +52,6 @@ static void fw_unit_get_property(GObject *obj, guint id,
 	HinawaFwUnitPrivate *priv = FW_UNIT_GET_PRIVATE(self);
 
 	switch (id) {
-	case FW_UNIT_PROP_TYPE_FD:
-		g_value_set_int(val, priv->fd);
-		break;
 	case FW_UNIT_PROP_TYPE_GENERATION:
 		g_value_set_uint64(val, priv->generation);
 		break;
@@ -71,9 +68,6 @@ static void fw_unit_set_property(GObject *obj, guint id,
 	HinawaFwUnitPrivate *priv = FW_UNIT_GET_PRIVATE(self);
 
 	switch (id) {
-	case FW_UNIT_PROP_TYPE_FD:
-		priv->fd = g_value_get_int(val);
-		break;
 	case FW_UNIT_PROP_TYPE_GENERATION:
 		priv->generation = g_value_get_uint64(val);
 		break;
@@ -88,8 +82,9 @@ static void fw_unit_dispose(GObject *obj)
 	HinawaFwUnit *self = HINAWA_FW_UNIT(obj);
 	HinawaFwUnitPrivate *priv = FW_UNIT_GET_PRIVATE(self);
 
-	if (priv->src != NULL)
-		hinawa_fw_unit_unlisten(self);
+	hinawa_fw_unit_unlisten(self);
+
+	close(priv->fd);
 
 	G_OBJECT_CLASS(hinawa_fw_unit_parent_class)->dispose(obj);
 }
@@ -108,11 +103,6 @@ static void hinawa_fw_unit_class_init(HinawaFwUnitClass *klass)
 	gobject_class->dispose = fw_unit_dispose;
 	gobject_class->finalize = fw_unit_finalize;
 
-	fw_unit_props[FW_UNIT_PROP_TYPE_FD] =
-		g_param_spec_int("fd", "fd",
-				 "a file descriptor for fw cdev.",
-				 INT_MIN, INT_MAX, 0,
-				 G_PARAM_READABLE);
 	fw_unit_props[FW_UNIT_PROP_TYPE_GENERATION] =
 		g_param_spec_uint64("generation", "generation",
 				    "current level of generation on this bus.",
@@ -141,6 +131,7 @@ static void hinawa_fw_unit_init(HinawaFwUnit *self)
 HinawaFwUnit *hinawa_fw_unit_new(gchar *path, GError **exception)
 {
 	HinawaFwUnit *self;
+	HinawaFwUnitPrivate *priv;
 	int fd;
 	struct fw_cdev_get_info info = {0};
 	struct fw_cdev_event_bus_reset br = {0};
@@ -158,6 +149,7 @@ HinawaFwUnit *hinawa_fw_unit_new(gchar *path, GError **exception)
 			    ENOMEM, "%s", strerror(ENOMEM));
 		return NULL;
 	}
+	priv = FW_UNIT_GET_PRIVATE(self);
 
 	info.version = 4;
 	info.bus_reset = (guint64)&br;
@@ -170,10 +162,22 @@ HinawaFwUnit *hinawa_fw_unit_new(gchar *path, GError **exception)
 		return NULL;
 	}
 
-	self->priv->fd = fd;
-	self->priv->generation = br.generation;
+	priv->fd = fd;
+	priv->generation = br.generation;
 
 	return self;
+}
+
+void hinawa_fw_unit_ioctl(HinawaFwUnit *self, int req, void *args, int *err)
+{
+	HinawaFwUnitPrivate *priv;
+
+	g_return_if_fail(HINAWA_IS_FW_UNIT(self));
+	priv = FW_UNIT_GET_PRIVATE(self);
+
+	*err = 0;
+	if (ioctl(priv->fd, req, args) < 0)
+		*err = errno;
 }
 
 static void handle_update(int fd, union fw_cdev_event *ev)
@@ -314,13 +318,14 @@ void hinawa_fw_unit_unlisten(HinawaFwUnit *self)
 	g_return_if_fail(HINAWA_IS_FW_UNIT(self));
 	priv = FW_UNIT_GET_PRIVATE(self);
 
+	if (priv->src == NULL)
+		return;
+
 	g_source_destroy((GSource *)priv->src);
 	g_free(priv->src);
 	priv->src = NULL;
 
-	close(priv->fd);
-
 	g_free(priv->buf);
 	priv->buf = NULL;
-	self->priv->len = 0;
+	priv->len = 0;
 }

src/fw_unit.h
@@ -3,7 +3,6 @@
 
 #include <glib.h>
 #include <glib-object.h>
-#include "internal.h"
 
 G_BEGIN_DECLS
 

src/internal.h
@@ -8,11 +8,13 @@
 #include <linux/firewire-constants.h>
 
 #include "backport.h"
+#include "fw_unit.h"
 #include "snd_unit.h"
 
 typedef void (*HinawaFwUnitHandle)(int fd, union fw_cdev_event *ev);
 void hinawa_fw_resp_handle_request(int fd, union fw_cdev_event *ev);
 void hinawa_fw_req_handle_response(int fd, union fw_cdev_event *ev);
+void hinawa_fw_unit_ioctl(HinawaFwUnit *self, int req, void *args, int *err);
 
 typedef void (HinawaSndUnitHandle)(void *private_data,
 				   const void *buf, unsigned int len);

commit 34350fbfcb07d2c4fb589978700ada5d9542ccf4

fw_resp: check just negative value returned from ioctl call
Fixes: 5306410ddaea ("fw_unit: add ioctl method for internal use")
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/fw_resp.c
@@ -169,7 +169,7 @@ void hinawa_fw_resp_reserve(HinawaFwResp *self, HinawaFwNode*node,
 	allocate.region_end = addr + width;
 
 	hinawa_fw_node_ioctl(node, FW_CDEV_IOC_ALLOCATE, &allocate, &err);
-	if (err != 0) {
+	if (err < 0) {
 		raise(exception, err);
 		return;
 	}