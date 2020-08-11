alsa-project/libhinawa

commit c8a0a9651afc7a6860f84fe0ea8c2e718c7c6e3a

fw_node: add an API to create GSource to listen events for node on IEEE 1394 bus

src/fw_node.c
@@ -44,6 +44,14 @@ G_DEFINE_QUARK("HinawaFwNode", hinawa_fw_node)
 	g_set_error(exception, hinawa_fw_node_quark(), errno,		\
 		    "%d: %s", __LINE__, strerror(errno))
 
+typedef struct {
+	GSource src;
+	HinawaFwNode *self;
+	gpointer tag;
+	unsigned int len;
+	void *buf;
+} FwNodeSource;
+
 enum fw_node_prop_type {
 	FW_NODE_PROP_TYPE_NODE_ID = 1,
 	FW_NODE_PROP_TYPE_LOCAL_NODE_ID,
@@ -261,4 +269,112 @@ void hinawa_fw_node_get_config_rom(HinawaFwNode *self, const guint8 **image,
 
 	*image = priv->config_rom;
 	*length = priv->config_rom_length;
+}
+
+static gboolean prepare_src(GSource *src, gint *timeout)
+{
+	// Use 500 msec for safe cancellation of thread.
+	*timeout = 500;
+
+	// This source is not ready, let's poll(2).
+	return FALSE;
+}
+
+static gboolean check_src(GSource *gsrc)
+{
+	FwNodeSource *src = (FwNodeSource *)gsrc;
+	GIOCondition condition;
+
+	condition = g_source_query_unix_fd(gsrc, src->tag);
+	if (condition & G_IO_ERR)
+		return FALSE;
+
+	// Don't go to dispatch if nothing available.
+	return !!(condition & G_IO_IN);
+}
+
+static gboolean dispatch_src(GSource *gsrc, GSourceFunc cb, gpointer user_data)
+{
+	FwNodeSource *src = (FwNodeSource *)gsrc;
+	HinawaFwNodePrivate *priv;
+	int len;
+
+	if (src->self == NULL || !HINAWA_IS_FW_NODE(src->self))
+		return G_SOURCE_REMOVE;
+	priv = hinawa_fw_node_get_instance_private(src->self);
+
+	if (priv->fd < 0)
+		return G_SOURCE_REMOVE;
+
+	len = read(priv->fd, src->buf, src->len);
+	if (len < 0) {
+		if (errno == EAGAIN)
+			return G_SOURCE_CONTINUE;
+
+		return G_SOURCE_REMOVE;
+	}
+
+	// Dispatch the event.
+
+	// Just be sure to continue to process this source.
+	return G_SOURCE_CONTINUE;
+}
+
+static void finalize_src(GSource *gsrc)
+{
+	FwNodeSource *src = (FwNodeSource *)gsrc;
+
+	g_free(src->buf);
+}
+
+/**
+ * hinawa_fw_node_create_source:
+ * @self: A #HinawaFwNode.
+ * @gsrc: (out): A #GSource.
+ * @exception: A #GError.
+ *
+ * Create Gsource for GMainContext to dispatch events for the node on IEEE 1394
+ * bus.
+ *
+ * Since: 1.4.
+ */
+void hinawa_fw_node_create_source(HinawaFwNode *self, GSource **gsrc,
+				  GError **exception)
+{
+        static GSourceFuncs funcs = {
+                .prepare        = prepare_src,
+                .check          = check_src,
+                .dispatch       = dispatch_src,
+                .finalize       = finalize_src,
+        };
+	HinawaFwNodePrivate *priv;
+	FwNodeSource *src;
+
+	g_return_if_fail(HINAWA_IS_FW_NODE(self));
+	priv = hinawa_fw_node_get_instance_private(self);
+
+	if (priv->fd < 0) {
+		raise(exception, ENXIO);
+		return;
+	}
+
+        *gsrc = g_source_new(&funcs, sizeof(FwNodeSource));
+	src = (FwNodeSource *)(*gsrc);
+
+        g_source_set_name(*gsrc, "HinawaFwNode");
+        g_source_set_priority(*gsrc, G_PRIORITY_HIGH_IDLE);
+        g_source_set_can_recurse(*gsrc, TRUE);
+
+        // MEMO: allocate one page because we cannot assume the size of
+        // transaction frame.
+        src->len = sysconf(_SC_PAGESIZE);
+        src->buf = g_malloc0(src->len);
+        if (src->buf == NULL) {
+                raise(exception, ENOMEM);
+		g_source_unref(*gsrc);
+                return;
+        }
+
+	src->self = self;
+	src->tag = g_source_add_unix_fd(*gsrc, priv->fd, G_IO_IN);
 }

src/fw_node.h
@@ -53,6 +53,9 @@ void hinawa_fw_node_open(HinawaFwNode *self, const gchar *path,
 void hinawa_fw_node_get_config_rom(HinawaFwNode *self, const guint8 **image,
 				   guint *length, GError **exception);
 
+void hinawa_fw_node_create_source(HinawaFwNode *self, GSource **gsrc,
+				  GError **exception);
+
 G_END_DECLS
 
 #endif

src/hinawa.map
@@ -103,4 +103,5 @@ HINAWA_1_4_0 {
     "hinawa_fw_node_quark";
     "hinawa_fw_node_open";
     "hinawa_fw_node_get_config_rom";
+    "hinawa_fw_node_create_source";
 };

tests/fw-node
@@ -22,6 +22,7 @@ methods = (
     'new',
     'open',
     'get_config_rom',
+    'create_source',
 )
 vmethods = ()
 signals = ()

commit 439bc8496cd758ae473672d035719ab1df763ecd

fw_node: release allocated GError in event loop
In event loop, error is not reported to users directly. Once GError is
allocated, it should be released, but not.

This commit fixes the bug.

Fixes: c8a0a9651afc ("fw_node: add an API to create GSource to listen events for node on IEEE 1394 bus")
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/fw_node.c
@@ -406,8 +406,10 @@ static gboolean dispatch_src(GSource *gsrc, GSourceFunc cb, gpointer user_data)
 		g_mutex_unlock(&priv->transactions_mutex);
 	}
 
-	if (exception != NULL)
+	if (exception != NULL) {
+		g_clear_error(&exception);
 		return G_SOURCE_REMOVE;
+	}
 
 	// Just be sure to continue to process this source.
 	return G_SOURCE_CONTINUE;