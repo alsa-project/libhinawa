alsa-project/libhinawa

commit 5692fc98abc0c5f9a81e197bf12835e89d778d2f

use glib quark macro and bump up glib dependency to 2.34.0 or later

src/fw_resp.c
@@ -18,6 +18,13 @@
  * This class is an application of Linux FireWire subsystem. All of operations
  * utilize ioctl(2) with subsystem specific request commands.
  */
+
+/* For error handling. */
+G_DEFINE_QUARK("HinawaFwResp", hinawa_fw_resp)
+#define raise(exception, errno)						\
+	g_set_error(exception, hinawa_fw_resp_quark(), errno,		\
+		    "%d: %s", __LINE__, strerror(errno))
+
 struct _HinawaFwRespPrivate {
 	HinawaFwUnit *unit;
 
@@ -112,8 +119,7 @@ void hinawa_fw_resp_register(HinawaFwResp *self, HinawaFwUnit *unit,
 	priv = FW_RESP_GET_PRIVATE(self);
 
 	if (priv->unit != NULL) {
-		g_set_error(exception, g_quark_from_static_string(__func__),
-			    EINVAL, "%s", strerror(EINVAL));
+		raise(exception, EINVAL);
 		return;
 	}
 	priv->unit = g_object_ref(unit);
@@ -125,8 +131,7 @@ void hinawa_fw_resp_register(HinawaFwResp *self, HinawaFwUnit *unit,
 
 	hinawa_fw_unit_ioctl(priv->unit, FW_CDEV_IOC_ALLOCATE, &allocate, &err);
 	if (err != 0) {
-		g_set_error(exception, g_quark_from_static_string(__func__),
-			    err, "%s", strerror(err));
+		raise(exception, err);
 		g_object_unref(priv->unit);
 		priv->unit = NULL;
 		return;
@@ -134,16 +139,14 @@ void hinawa_fw_resp_register(HinawaFwResp *self, HinawaFwUnit *unit,
 
 	priv->buf = g_malloc0(allocate.length);
 	if (priv->buf == NULL) {
-		g_set_error(exception, g_quark_from_static_string(__func__),
-			    ENOMEM, "%s", strerror(ENOMEM));
+		raise(exception, ENOMEM);
 		hinawa_fw_resp_unregister(self);
 		return;
 	}
 
 	priv->req_frame  = g_array_new(FALSE, TRUE, sizeof(guint32));
 	if (priv->req_frame == NULL) {
-		g_set_error(exception, g_quark_from_static_string(__func__),
-			    ENOMEM, "%s", strerror(ENOMEM));
+		raise(exception, ENOMEM);
 		hinawa_fw_resp_unregister(self);
 		return;
 	}

README
@@ -2,7 +2,7 @@ Requirements
 - GNU Autoconf 2.62 or later
 - GNU Automake 1.10.1 or later
 - GNU libtool 2.2.6 or later
-- Glib 2.32.4 or later
+- Glib 2.34.0 or later
 - GTK-Doc 1.18-2
 - GObject Introspection 1.32.1 or later
 - Linux kernel 3.12 or later

configure.ac
@@ -44,7 +44,7 @@ AC_SUBST(LT_IFACE)
 AC_PROG_CC
 
 # Glib 2.32 or later
-AM_PATH_GLIB_2_0([2.32.4], [], [], [gobject])
+AM_PATH_GLIB_2_0([2.34.0], [], [], [gobject])
 
 # GTK-Doc 1.18-2 or later
 GTK_DOC_CHECK([1.18-2])

src/fw_fcp.c
@@ -16,6 +16,12 @@
  * This class is an application of #HinawaFwReq / #HinawaFwResp.
  */
 
+/* For error handling. */
+G_DEFINE_QUARK("HinawaFwFcp", hinawa_fw_fcp)
+#define raise(exception, errno)						\
+	g_set_error(exception, hinawa_fw_fcp_quark(), errno,		\
+		    "%d: %s", __LINE__, strerror(errno))
+
 #define FCP_MAXIMUM_FRAME_BYTES	0x200U
 #define FCP_REQUEST_ADDR	0xfffff0000b00
 #define FCP_RESPOND_ADDR	0xfffff0000d00
@@ -112,8 +118,7 @@ void hinawa_fw_fcp_transact(HinawaFwFcp *self,
 	if (req_frame  == NULL || g_array_get_element_size(req_frame)  != 1 ||
 	    resp_frame == NULL || g_array_get_element_size(resp_frame) != 1 ||
 	    req_frame->len > FCP_MAXIMUM_FRAME_BYTES) {
-		g_set_error(exception, g_quark_from_static_string(__func__),
-			    EINVAL, "%s", strerror(EINVAL));
+		raise(exception, EINVAL);
 		return;
 	}
 
@@ -155,8 +160,7 @@ deferred:
 	 */
 	g_mutex_lock(&local_lock);
 	if (!g_cond_wait_until(&trans.cond, &local_lock, expiration))
-		g_set_error(exception, g_quark_from_static_string(__func__),
-			    ETIMEDOUT, "%s", strerror(ETIMEDOUT));
+		raise(exception, ETIMEDOUT);
 	g_mutex_unlock(&local_lock);
 
 	/* Error happened. */

src/fw_req.c
@@ -21,6 +21,12 @@
  * utilize ioctl(2) with subsystem specific request commands.
  */
 
+/* For error handling. */
+G_DEFINE_QUARK("HinawaFwReq", hinawa_fw_req)
+#define raise(exception, errno)						\
+	g_set_error(exception, hinawa_fw_req_quark(), errno,		\
+		    "%d: %s", __LINE__, strerror(errno))
+
 enum fw_req_type {
 	FW_REQ_TYPE_WRITE = 0,
 	FW_REQ_TYPE_READ,
@@ -176,8 +182,7 @@ void hinawa_fw_req_write(HinawaFwReq *self, HinawaFwUnit *unit, guint64 addr,
 	}
 
 	if (err != 0)
-		g_set_error(exception, g_quark_from_static_string(__func__),
-			    err, "%s", strerror(err));
+		raise(exception, err);
 }
 
 /**
@@ -209,8 +214,7 @@ void hinawa_fw_req_read(HinawaFwReq *self, HinawaFwUnit *unit, guint64 addr,
 	}
 
 	if (err != 0)
-		g_set_error(exception, g_quark_from_static_string(__func__),
-			    err, "%s", strerror(err));
+		raise(exception, err);
 }
 
 /**
@@ -240,8 +244,7 @@ void hinawa_fw_req_lock(HinawaFwReq *self, HinawaFwUnit *unit,
 	}
 
 	if (err != 0)
-		g_set_error(exception, g_quark_from_static_string(__func__),
-			    err, "%s", strerror(err));
+		raise(exception, err);
 }
 
 /* NOTE: For HinawaFwUnit, internal. */

src/fw_unit.c
@@ -12,6 +12,12 @@
 #  include <config.h>
 #endif
 
+/* For error handling. */
+G_DEFINE_QUARK("HinawaFwUnit", hinawa_fw_unit)
+#define raise(exception, errno)						\
+	g_set_error(exception, hinawa_fw_unit_quark(), errno,		\
+		    "%d: %s", __LINE__, strerror(errno))
+
 /**
  * SECTION:fw_unit
  * @Title: HinawaFwUnit
@@ -179,8 +185,7 @@ void hinawa_fw_unit_open(HinawaFwUnit *self, gchar *path, GError **exception)
 
 	fd = open(path, O_RDONLY);
 	if (fd < 0) {
-		g_set_error(exception, g_quark_from_static_string(__func__),
-			    errno, "%s", strerror(errno));
+		raise(exception, errno);
 		return;
 	}
 
@@ -188,8 +193,7 @@ void hinawa_fw_unit_open(HinawaFwUnit *self, gchar *path, GError **exception)
 	info.bus_reset = (guint64)&br;
 	info.bus_reset_closure = (guint64)self;
 	if (ioctl(fd, FW_CDEV_IOC_GET_INFO, &info) < 0) {
-		g_set_error(exception, g_quark_from_static_string(__func__),
-			    errno, "%s", strerror(errno));
+		raise(exception, errno);
 		close(fd);
 		return;
 	}
@@ -314,15 +318,13 @@ void hinawa_fw_unit_listen(HinawaFwUnit *self, GError **exception)
 	 */
 	buf = g_malloc0(getpagesize());
 	if (buf == NULL) {
-		g_set_error(exception, g_quark_from_static_string(__func__),
-			    ENOMEM, "%s", strerror(ENOMEM));
+		raise(exception, ENOMEM);
 		return;
 	}
 
 	src = g_source_new(&funcs, sizeof(FwUnitSource));
 	if (src == NULL) {
-		g_set_error(exception, g_quark_from_static_string(__func__),
-			    ENOMEM, "%s", strerror(ENOMEM));
+		raise(exception, ENOMEM);
 		g_free(buf);
 		return;
 	}

src/snd_dg00x.c
@@ -18,6 +18,12 @@
 
 G_DEFINE_TYPE(HinawaSndDg00x, hinawa_snd_dg00x, HINAWA_TYPE_SND_UNIT)
 
+/* For error handling. */
+G_DEFINE_QUARK("HinawaSndDg00x", hinawa_snd_dg00x)
+#define raise(exception, errno)						\
+	g_set_error(exception, hinawa_snd_dg00x_quark(), errno,		\
+		    "%d: %s", __LINE__, strerror(errno))
+
 /* This object has one signal. */
 enum dg00x_sig_type {
 	DG00X_SIG_TYPE_MESSAGE,
@@ -85,8 +91,7 @@ void hinawa_snd_dg00x_open(HinawaSndDg00x *self, gchar *path, GError **exception
 
 	g_object_get(G_OBJECT(self), "type", &type, NULL);
 	if (type != SNDRV_FIREWIRE_TYPE_DIGI00X) {
-		g_set_error(exception, g_quark_from_static_string(__func__),
-			    EINVAL, "%s", strerror(EINVAL));
+		raise(exception, EINVAL);
 		return;
 	}
 }

src/snd_dice.c
@@ -16,6 +16,12 @@
  * received. This inherits #HinawaSndUnit.
  */
 
+/* For error handling. */
+G_DEFINE_QUARK("HinawaSndDice", hinawa_snd_dice)
+#define raise(exception, errno)						\
+	g_set_error(exception, hinawa_snd_dice_quark(), errno,		\
+		    "%d: %s", __LINE__, strerror(errno))
+
 struct notification_waiter {
 	guint32 bit_flag;
 	GCond cond;
@@ -99,8 +105,7 @@ void hinawa_snd_dice_open(HinawaSndDice *self, gchar *path, GError **exception)
 
 	g_object_get(G_OBJECT(self), "type", &type, NULL);
 	if (type != SNDRV_FIREWIRE_TYPE_DICE) {
-		g_set_error(exception, g_quark_from_static_string(__func__),
-			    EINVAL, "%s", strerror(EINVAL));
+		raise(exception, EINVAL);
 		return;
 	}
 
@@ -149,8 +154,7 @@ void hinawa_snd_dice_transact(HinawaSndDice *self, guint64 addr,
 	 * section.
 	 */
 	if (!g_cond_wait_until(&waiter.cond, &priv->lock, expiration))
-		g_set_error(exception, g_quark_from_static_string(__func__),
-			    ETIMEDOUT, "%s", strerror(ETIMEDOUT));
+		raise(exception, ETIMEDOUT);
 end:
 	priv->waiters = g_list_remove(priv->waiters, (gpointer *)&waiter);
 

commit d17cf98a054e25991291ae97884f77d1b83a0e2a

fw_resp: fix invalid assignment of errno to GError
The call of hinawa_fw_node_ioctl() assigns negative value to given pointer
for error code. Although the code in GError should be positive value,
the negative value is assigned to the code as is.

This commit fixes the bug.

Fixes: 5692fc98abc0 ("use glib quark macro and bump up glib dependency to 2.34.0 or later")
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/fw_resp.c
@@ -171,7 +171,7 @@ void hinawa_fw_resp_reserve(HinawaFwResp *self, HinawaFwNode*node,
 
 	hinawa_fw_node_ioctl(node, FW_CDEV_IOC_ALLOCATE, &allocate, &err);
 	if (err < 0) {
-		raise(exception, err);
+		raise(exception, -err);
 		return;
 	}
 	priv->node = g_object_ref(node);