alsa-project/libhinawa

commit c1c1c405d229eb4ee487d528f3efeabe8cd6ab82

fw_fcp: error reporting enhancement
In guideline of GLib's GError, the naming scheme of GQuark for
error domain is <namespace>_<module>_error_quark. Additionally,
GLib enumeration should be implemented for error code and the naming
scheme is <NAMESPACE>_<MODULE>_ERROR_<CODE>. GError should be
instanciated just for recoverable runtime error.

Current implementation of GError for HinawaFwFcp is against the
above. This commit obsoletes current GQuark and add new one. A function
is added for applications to access to it. This commit adds enumeration
for error code as well. The error code is just used by GError instance
with domain of the GQuark.

Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/hinawa_enum_types.h
@@ -122,4 +122,14 @@ typedef enum {
 	HINAWA_FW_NODE_ERROR_NOT_OPENED,
 } HinawaFwNodeError;
 
+/**
+ * HinawaFwFcpError:
+ * @HINAWA_FW_FCP_ERROR_TIMEOUT:	The transaction is canceled due to response timeout.
+ * @HINAWA_FW_FCP_ERROR_LARGE_RESP:	The size of response is larger than expected.
+ */
+typedef enum {
+	HINAWA_FW_FCP_ERROR_TIMEOUT,
+	HINAWA_FW_FCP_ERROR_LARGE_RESP,
+} HinawaFwFcpError;
+
 #endif

src/fw_fcp.c
@@ -20,11 +20,22 @@
  * This class is an application of #HinawaFwReq / #HinawaFwResp.
  */
 
-/* For error handling. */
-G_DEFINE_QUARK("HinawaFwFcp", hinawa_fw_fcp)
-#define raise(exception, errno)						\
-	g_set_error(exception, hinawa_fw_fcp_quark(), errno,		\
-		    "%d: %s", __LINE__, strerror(errno))
+/**
+ * hinawa_fw_fcp_error_quark:
+ *
+ * Return the GQuark for error domain of GError which has code in #HinawaFwFcpError.
+ *
+ * Returns: A #GQuark.
+ */
+G_DEFINE_QUARK(hinawa-fw-fcp-error-quark, hinawa_fw_fcp_error)
+
+static const char *const local_err_msgs[] = {
+	[HINAWA_FW_FCP_ERROR_TIMEOUT]		= "The transaction is canceled due to response timeout",
+	[HINAWA_FW_FCP_ERROR_LARGE_RESP]	= "The size of response is larger than expected",
+};
+
+#define generate_local_error(exception, code)						\
+	g_set_error_literal(exception, HINAWA_FW_FCP_ERROR, code, local_err_msgs[code])
 
 #define FCP_MAXIMUM_FRAME_BYTES	0x200U
 #define FCP_REQUEST_ADDR	0xfffff0000b00
@@ -189,7 +200,9 @@ HinawaFwFcp *hinawa_fw_fcp_new(void)
  * @resp_frame_size: The size of array for response in byte unit. The value of
  *		     this argument should point to the numerical number and
  *		     mutable.
- * @exception: A #GError.
+ * @exception: A #GError. Error can be generated with four domains; #g_file_error_quark(),
+ *	       #hinawa_fw_node_error_quark(), #hinawa_fw_req_error_quark(), and
+ *	       #hinawa_fw_fcp_error_quark().
  *
  * Execute FCP transaction.
  * Since: 1.4.
@@ -259,7 +272,7 @@ deferred:
 			break;
 	}
 	if (trans.resp_frame[0] == 0x00) {
-		raise(exception, ETIMEDOUT);
+		generate_local_error(exception, HINAWA_FW_FCP_ERROR_TIMEOUT);
 	} else if (trans.resp_frame[0] == AVC_STATUS_INTERIM) {
 		// It's a deffered transaction, wait again.
 		trans.resp_frame[0] = 0x00;
@@ -268,10 +281,11 @@ deferred:
 		goto deferred;
 	}
 
-	if (trans.resp_frame_size > NOTIFY_ENOBUFS)
+	if (trans.resp_frame_size > NOTIFY_ENOBUFS) {
 		*resp_frame_size = trans.resp_frame_size;
-	else
-		raise(exception, ENOBUFS);
+	} else {
+		generate_local_error(exception, HINAWA_FW_FCP_ERROR_LARGE_RESP);
+	}
 end:
 	g_mutex_unlock(&trans.mutex);
 	g_cond_clear(&trans.cond);

src/fw_fcp.h
@@ -33,6 +33,10 @@ G_BEGIN_DECLS
 				   HINAWA_TYPE_FW_FCP,		\
 				   HinawaFwFcpClass))
 
+#define HINAWA_FW_FCP_ERROR	hinawa_fw_fcp_error_quark()
+
+GQuark hinawa_fw_fcp_error_quark();
+
 typedef struct _HinawaFwFcp		HinawaFwFcp;
 typedef struct _HinawaFwFcpClass	HinawaFwFcpClass;
 typedef struct _HinawaFwFcpPrivate	HinawaFwFcpPrivate;

src/hinawa.map
@@ -5,7 +5,6 @@ HINAWA_0_6_0 {
     "hinawa_fw_resp_get_type";
 
     "hinawa_fw_fcp_get_type";
-    "hinawa_fw_fcp_quark";
 
     "hinawa_snd_unit_get_type";
     "hinawa_snd_unit_open";
@@ -103,4 +102,7 @@ HINAWA_2_1_0 {
     "hinawa_fw_node_error_quark";
 
     "hinawa_fw_req_error_quark";
+
+    "hinawa_fw_fcp_error_get_type";
+    "hinawa_fw_fcp_error_quark";
 } HINAWA_2_0_0;

tests/hinawa-enum
@@ -60,11 +60,17 @@ fw_node_error_enumerators = (
     'NOT_OPENED',
 )
 
+fw_fcp_error_enumerators = (
+    'TIMEOUT',
+    'LARGE_RESP',
+)
+
 types = {
     Hinawa.FwTcode: fw_tcode_enumerators,
     Hinawa.FwRcode: fw_rcode_enumerators,
     Hinawa.SndUnitType: snd_unit_type_enumerators,
     Hinawa.FwNodeError: fw_node_error_enumerators,
+    Hinawa.FwFcpError: fw_fcp_error_enumerators,
 }
 
 for obj, types in types.items():

commit f2cf1cabb6633a6dfb3ad4427866cfe6b914fb23

enum: fix documentation
Fixes: c1c1c405d229 ("fw_fcp: error reporting enhancement")
Fixes: ea676b10efd3 ("snd_unit: error reporting enhancement")
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/hinawa_enum_types.h
@@ -128,6 +128,8 @@ typedef enum {
  * HinawaFwFcpError:
  * @HINAWA_FW_FCP_ERROR_TIMEOUT:	The transaction is canceled due to response timeout.
  * @HINAWA_FW_FCP_ERROR_LARGE_RESP:	The size of response is larger than expected.
+ *
+ * A set of error code for GError with domain which equals to #hinawa_fw_fcp_error_quark().
  */
 typedef enum {
 	HINAWA_FW_FCP_ERROR_TIMEOUT,
@@ -147,7 +149,7 @@ typedef enum {
  * @HINAWA_SND_UNIT_ERROR_WRONG_CLASS:	The hwdep device is not for the unit expected by the class.
  * @HINAWA_SND_UNIT_ERROR_FAILED:	The system call fails.
  *
- * A set of error code for GError with domain of #HinawaSndUnitError.
+ * A set of error code for GError with domain which equals to #hinawa_snd_unit_error_quark().
  */
 typedef enum {
 	HINAWA_SND_UNIT_ERROR_DISCONNECTED,