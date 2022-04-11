6204c36aa79bb16b86d2aa534315e3a8ec775690
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2022-04-11 17:41:05 +0900

    fw_fcp: use an utility macro to declare GObject-derived object
    Since gobject v2.44, an utility macro is available to declare
    GObject-derived objects.
    
    This commit replaces existent boireplates with it.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- src/fw_fcp.h
+++ src/fw_fcp.h
@@ -8,39 +8,11 @@ G_BEGIN_DECLS
 
 #define HINAWA_TYPE_FW_FCP	(hinawa_fw_fcp_get_type())
 
-#define HINAWA_FW_FCP(obj)					\
-	(G_TYPE_CHECK_INSTANCE_CAST((obj),			\
-				    HINAWA_TYPE_FW_FCP,		\
-				    HinawaFwFcp))
-#define HINAWA_IS_FW_FCP(obj)					\
-	(G_TYPE_CHECK_INSTANCE_TYPE((obj),			\
-				    HINAWA_TYPE_FW_FCP))
-
-#define HINAWA_FW_FCP_CLASS(klass)				\
-	(G_TYPE_CHECK_CLASS_CAST((klass),			\
-				 HINAWA_TYPE_FW_FCP,		\
-				 HinawaFwFcpClass))
-#define HINAWA_IS_FW_FCP_CLASS(klass)				\
-	(G_TYPE_CHECK_CLASS_TYPE((klass),			\
-				 HINAWA_TYPE_FW_FCP))
-#define HINAWA_FW_FCP_GET_CLASS(obj)				\
-	(G_TYPE_INSTANCE_GET_CLASS((obj),			\
-				   HINAWA_TYPE_FW_FCP,		\
-				   HinawaFwFcpClass))
+G_DECLARE_DERIVABLE_TYPE(HinawaFwFcp, hinawa_fw_fcp, HINAWA, FW_FCP, HinawaFwResp);
 
 #define HINAWA_FW_FCP_ERROR	hinawa_fw_fcp_error_quark()
 
 GQuark hinawa_fw_fcp_error_quark();
-
-typedef struct _HinawaFwFcp		HinawaFwFcp;
-typedef struct _HinawaFwFcpClass	HinawaFwFcpClass;
-typedef struct _HinawaFwFcpPrivate	HinawaFwFcpPrivate;
-
-struct _HinawaFwFcp {
-	HinawaFwResp parent_instance;
-
-	HinawaFwFcpPrivate *priv;
-};
 
 struct _HinawaFwFcpClass {
 	HinawaFwRespClass parent_class;
@@ -61,8 +33,6 @@ struct _HinawaFwFcpClass {
          */
 	void (*responded)(HinawaFwFcp *self, const guint8 *frame, guint frame_size);
 };
-
-GType hinawa_fw_fcp_get_type(void) G_GNUC_CONST;
 
 HinawaFwFcp *hinawa_fw_fcp_new(void);
 
