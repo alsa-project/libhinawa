ffe24a6cef966dfd2ff2843a8d68c6f6130cdf6a
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2022-04-11 17:41:05 +0900

    fw_node: use an utility macro to declare GObject-derived object
    Since gobject v2.44, an utility macro is available to declare
    GObject-derived objects.
    
    This commit replaces existent boireplates with it.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- src/fw_node.h
+++ src/fw_node.h
@@ -8,39 +8,11 @@ G_BEGIN_DECLS
 
 #define HINAWA_TYPE_FW_NODE	(hinawa_fw_node_get_type())
 
-#define HINAWA_FW_NODE(obj)					\
-	(G_TYPE_CHECK_INSTANCE_CAST((obj),			\
-				    HINAWA_TYPE_FW_NODE,	\
-				    HinawaFwNode))
-#define HINAWA_IS_FW_NODE(obj)					\
-	(G_TYPE_CHECK_INSTANCE_TYPE((obj),			\
-				    HINAWA_TYPE_FW_NODE))
-
-#define HINAWA_FW_NODE_CLASS(klass)				\
-	(G_TYPE_CHECK_CLASS_CAST((klass),			\
-				 HINAWA_TYPE_FW_NODE,		\
-				 HinawaFwNodeClass))
-#define HINAWA_IS_FW_NODE_CLASS(klass)				\
-	(G_TYPE_CHECK_CLASS_TYPE((klass),			\
-				 HINAWA_TYPE_FW_NODE))
-#define HINAWA_FW_NODE_GET_CLASS(obj)				\
-	(G_TYPE_INSTANCE_GET_CLASS((obj),			\
-				   HINAWA_TYPE_FW_NODE,		\
-				   HinawaFwNodeClass))
+G_DECLARE_DERIVABLE_TYPE(HinawaFwNode, hinawa_fw_node, HINAWA, FW_NODE, GObject);
 
 #define HINAWA_FW_NODE_ERROR	hinawa_fw_node_error_quark()
 
 GQuark hinawa_fw_node_error_quark();
-
-typedef struct _HinawaFwNode		HinawaFwNode;
-typedef struct _HinawaFwNodeClass	HinawaFwNodeClass;
-typedef struct _HinawaFwNodePrivate	HinawaFwNodePrivate;
-
-struct _HinawaFwNode {
-	GObject parent_instance;
-
-	HinawaFwNodePrivate *priv;
-};
 
 struct _HinawaFwNodeClass {
 	GObjectClass parent_class;
@@ -69,8 +41,6 @@ struct _HinawaFwNodeClass {
 	void (*disconnected)(HinawaFwNode *self);
 
 };
-
-GType hinawa_fw_node_get_type(void) G_GNUC_CONST;
 
 HinawaFwNode *hinawa_fw_node_new(void);
 
