alsa-project/libhinawa

commit b6a463973655930635a33ed524d8e08153923ca9

fw_resp: add 'requested2' GObject signal to handle response subaction of transaction
The 'struct fw_cdev_event_requested2' in UAPI of Linux firewire subsystem has
more members than used for 'requested' GObject signal implemented in
HinawaFwResp.

This commit add another GObject signal 'requested2' to pass the members to
handlers as arguments.

Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/fw_resp.c
@@ -63,6 +63,7 @@ static GParamSpec *fw_resp_props[FW_RESP_PROP_TYPE_COUNT] = { NULL, };
 // This object has one signal.
 enum fw_resp_sig_type {
 	FW_RESP_SIG_TYPE_REQ = 0,
+	FW_RESP_SIG_TYPE_REQ2,
 	FW_RESP_SIG_TYPE_COUNT,
 };
 static guint fw_resp_sigs[FW_RESP_SIG_TYPE_COUNT] = { 0 };
@@ -131,6 +132,38 @@ static void hinawa_fw_resp_class_init(HinawaFwRespClass *klass)
 			     NULL, NULL,
 			     hinawa_sigs_marshal_ENUM__ENUM,
 			     HINAWA_TYPE_FW_RCODE, 1, HINAWA_TYPE_FW_TCODE);
+
+	/**
+	 * HinawaFwResp::requested2:
+	 * @self: A #HinawaFwResp
+	 * @tcode: One of #HinawaTcode enumerations
+	 * @offset: The address offset at which the transaction arrives.
+	 * @src: The node ID of source for the transaction.
+	 * @dst: The node ID of destination for the transaction.
+	 * @card: The index of card corresponding to 1394 OHCI controller.
+	 * @generation: The generation of bus when the transaction is transferred.
+	 * @frame: (element-type guint8)(array length=length): The array with elements for byte
+	 *	   data.
+	 * @length: The length of bytes for the frame.
+	 *
+	 * When any node transfers request subaction to the range of address to which this object
+	 * listening, the #HinawaFwResp::requested signal handler is called with arrived frame for
+	 * the subaction. The handler is expected to call #hinawa_fw_resp_set_resp_frame() with
+	 * frame and return rcode for response subaction.
+	 *
+	 * Returns: One of #HinawaRcode enumerators corresponding to rcodes defined in IEEE 1394
+	 *	    specification.
+	 */
+	fw_resp_sigs[FW_RESP_SIG_TYPE_REQ2] =
+		g_signal_new("requested2",
+			     G_OBJECT_CLASS_TYPE(klass),
+			     G_SIGNAL_RUN_LAST,
+			     0,
+			     NULL, NULL,
+			     hinawa_sigs_marshal_ENUM__ENUM_UINT64_UINT_UINT_UINT_UINT_POINTER_UINT,
+			     HINAWA_TYPE_FW_RCODE, 8, HINAWA_TYPE_FW_TCODE, G_TYPE_UINT64,
+			     G_TYPE_UINT, G_TYPE_UINT, G_TYPE_UINT, G_TYPE_UINT,
+			     G_TYPE_POINTER, G_TYPE_UINT);
 }
 
 static void hinawa_fw_resp_init(HinawaFwResp *self)

src/hinawa_sigs_marshal.list
@@ -3,3 +3,4 @@ VOID:UINT,UINT,UINT
 VOID:ENUM,POINTER,UINT
 VOID:ENUM,UINT,UINT,UINT,POINTER,UINT
 VOID:POINTER,UINT
+ENUM:ENUM,UINT64,UINT,UINT,UINT,UINT,POINTER,UINT

tests/fw-resp
@@ -25,6 +25,7 @@ vmethods = (
 )
 signals = (
     'requested',
+    'requested2',
 )
 
 if not test(target, props, methods, vmethods, signals):

commit 33599b05235381c0d1e9944b0cc0cc034b5aade2

fw_resp: add deprecated flag for FwResp::requested
FwResp::requested2 should be used instead.

Fixes: b6a46397365 ("fw_resp: add 'requested2' GObject signal to handle response subaction of transaction")
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/fw_resp.c
@@ -170,7 +170,7 @@ static void hinawa_fw_resp_class_init(HinawaFwRespClass *klass)
 	fw_resp_sigs[FW_RESP_SIG_TYPE_REQ] =
 		g_signal_new("requested",
 			     G_OBJECT_CLASS_TYPE(klass),
-			     G_SIGNAL_RUN_LAST,
+			     G_SIGNAL_RUN_LAST | G_SIGNAL_DEPRECATED,
 			     G_STRUCT_OFFSET(HinawaFwRespClass, requested),
 			     NULL, NULL,
 			     hinawa_sigs_marshal_ENUM__ENUM,