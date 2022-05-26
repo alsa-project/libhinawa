alsa-project/libhinawa

commit 12881a0b1af33a0ebabe893311b8c486e4885dc9

snd_*: add deprecation note and flag to delegate to libhitaki
All of HinawaSnd* object classes and structures are supported by libhitaki
instead. Let's obsolete current implementations.

 * https://github.com/alsa-project/libhitaki

Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/snd_dg00x.c
@@ -9,6 +9,8 @@
  * A notification listener for Digidesign Digi 00x models.
  *
  * A [class@SndDg00x] listen to Dg00x notification and generates signal when received.
+ *
+ * Deprecated: 2.5. libhitaki library provides [class@Hitaki.SndDigi00x] as the alternative.
  */
 
 G_DEFINE_TYPE(HinawaSndDg00x, hinawa_snd_dg00x, HINAWA_TYPE_SND_UNIT)
@@ -30,11 +32,13 @@ static void hinawa_snd_dg00x_class_init(HinawaSndDg00xClass *klass)
 	 * Emitted when Dg00x models transfer notification.
 	 *
 	 * Since: 0.7
+	 * Deprecated: 2.5. Use implementation of [signal@Hitaki.AlsaFirewire::notified] in
+	 *	       [class@Hitaki.SndDigi00x] instead.
 	 */
 	dg00x_sigs[DG00X_SIG_TYPE_MESSAGE] =
 		g_signal_new("message",
 			     G_OBJECT_CLASS_TYPE(klass),
-			     G_SIGNAL_RUN_LAST,
+			     G_SIGNAL_RUN_LAST | G_SIGNAL_DEPRECATED,
 			     G_STRUCT_OFFSET(HinawaSndDg00xClass, message),
 			     NULL, NULL,
 			     g_cclosure_marshal_VOID__UINT,
@@ -52,7 +56,9 @@ static void hinawa_snd_dg00x_init(HinawaSndDg00x *self)
  * Instantiate [class@SndDg00x] object and return the instance.
  *
  * Returns: an instance of [class@SndDg00x].
+ *
  * Since: 1.3.
+ * Deprecated: 2.5. Use [method@Hitaki.SndDigi00x.new] instead.
  */
 HinawaSndDg00x *hinawa_snd_dg00x_new(void)
 {
@@ -69,6 +75,8 @@ HinawaSndDg00x *hinawa_snd_dg00x_new(void)
  * Open ALSA hwdep character device and check it for Dg00x devices.
  *
  * Since: 0.7
+ * Deprecated: 2.5. Use implementation of [method@Hitaki.AlsaFirewire.open]
+ *	       [class@Hitaki.SndDigi00x] for instead.
  */
 void hinawa_snd_dg00x_open(HinawaSndDg00x *self, gchar *path, GError **error)
 {

src/snd_dg00x.h
@@ -21,6 +21,8 @@ struct _HinawaSndDg00xClass {
 	 * Class closure for the [signal@SndDg00x::message] signal.
 	 *
 	 * Since: 1.2
+	 * Deprecated: 2.5. Use implementation of [vfunc@Hitaki.AlsaFirewire.notified] in
+	 *	       [class@Hitaki.SndDigi00x] instead.
 	 */
 	void (*message)(HinawaSndDg00x *self, guint32 message);
 };

src/snd_unit.c
@@ -18,6 +18,8 @@
  *
  * This class is an application of ALSA FireWire stack. Any functionality which ALSA drivers in the
  * stack can be available.
+ *
+ * Deprecated: 2.5. libhitaki library provides [class@Hitaki.SndUnit] as the alternative.
  */
 
 /**
@@ -26,6 +28,7 @@
  * Return the [alias@GLib.Quark] for [struct@GLib.Error] which has code in Hinawa.SndUnitError.
  *
  * Since: 2.1
+ * Deprecated: 2.5. Use Hitaki.AlsaFirewireError instead.
  *
  * Returns: A [alias@GLib.Quark].
  */
@@ -139,59 +142,64 @@ static void hinawa_snd_unit_class_init(HinawaSndUnitClass *klass)
 	 * HinawaSndUnit:type:
 	 *
 	 * Since: 1.0
+	 * Deprecated: 2.5. Use [property@Hitaki.AlsaFirewire:unit-type] instead.
 	 */
 	snd_unit_props[SND_UNIT_PROP_TYPE_FW_TYPE] =
 		g_param_spec_enum("type", "type",
 				  "The value of HinawaSndUnitType enumerators",
 				  HINAWA_TYPE_SND_UNIT_TYPE,
 				  HINAWA_SND_UNIT_TYPE_DICE,
-				  G_PARAM_READABLE);
+				  G_PARAM_READABLE | G_PARAM_DEPRECATED);
 
 	/**
 	 * HinawaSndUnit:card:
 	 *
 	 * Since: 2.0
+	 * Deprecated: 2.5. Use [property@Hitaki.AlsaFirewire:card] instead.
 	 */
 	snd_unit_props[SND_UNIT_PROP_TYPE_CARD_ID] =
 		g_param_spec_uint("card", "card",
 				  "The numeric ID for ALSA sound card",
 				  0, G_MAXUINT,
 				  0,
-				  G_PARAM_READABLE);
+				  G_PARAM_READABLE | G_PARAM_DEPRECATED);
 
 	/**
 	 * HinawaSndUnit:device:
 	 *
 	 *
 	 * Since: 0.3
+	 * Deprecated: 2.5. Use [property@Hitaki.AlsaFirewire:node-device] instead.
 	 */
 	snd_unit_props[SND_UNIT_PROP_TYPE_DEVICE] =
 		g_param_spec_string("device", "device",
 				    "A name of special file as FireWire unit.",
 				    NULL,
-				    G_PARAM_READABLE);
+				    G_PARAM_READABLE | G_PARAM_DEPRECATED);
 
 	/**
 	 * HinawaSndUnit:streaming:
 	 *
 	 * Since: 0.4
+	 * Deprecated: 2.5. Use [property@Hitaki.AlsaFirewire:is-locked] instead.
 	 */
 	snd_unit_props[SND_UNIT_PROP_TYPE_STREAMING] =
 		g_param_spec_boolean("streaming", "streaming",
 				     "Whether this device is streaming or not",
 				     FALSE,
-				     G_PARAM_READABLE);
+				     G_PARAM_READABLE | G_PARAM_DEPRECATED);
 
 	/**
 	 * HinawaSndUnit:guid:
 	 *
 	 * Since: 0.4
+	 * Deprecated: 2.5. Use [property@Hitaki.AlsaFirewire:guid] instead.
 	 */
 	snd_unit_props[SND_UNIT_PROP_TYPE_GUID] =
 		g_param_spec_uint64("guid", "guid",
 				    "Global unique ID for this firewire unit.",
 				    0, G_MAXUINT64, 0,
-				    G_PARAM_READABLE);
+				    G_PARAM_READABLE | G_PARAM_DEPRECATED);
 
 	g_object_class_install_properties(gobject_class,
 					  SND_UNIT_PROP_TYPE_COUNT,
@@ -206,11 +214,13 @@ static void hinawa_snd_unit_class_init(HinawaSndUnitClass *klass)
 	 * packet streaming requested by ALSA PCM/Rawmidi applications.
 	 *
 	 * Since: 0.3
+	 * Deprecated: 2.5. Use change notify signal of [property@Hitaki.AlsaFirewire:is-locked]
+	 *	       instead.
 	 */
 	snd_unit_sigs[SND_UNIT_SIG_TYPE_LOCK_STATUS] =
 		g_signal_new("lock-status",
 			     G_OBJECT_CLASS_TYPE(klass),
-			     G_SIGNAL_RUN_LAST,
+			     G_SIGNAL_RUN_LAST | G_SIGNAL_DEPRECATED,
 			     G_STRUCT_OFFSET(HinawaSndUnitClass, lock_status),
 			     NULL, NULL,
 			     g_cclosure_marshal_VOID__BOOLEAN,
@@ -225,11 +235,13 @@ static void hinawa_snd_unit_class_init(HinawaSndUnitClass *klass)
 	 * quickly as possible to release ALSA hwdep character device.
 	 *
 	 * Since: 2.0
+	 * Deprecated: 2.5. Use change notify signal of
+	 *	       [property@Hitaki.AlsaFirewire:is-disconnected] instead.
 	 */
 	snd_unit_sigs[SND_UNIT_SIG_TYPE_DISCONNECTED] =
 		g_signal_new("disconnected",
 			     G_OBJECT_CLASS_TYPE(klass),
-			     G_SIGNAL_RUN_LAST,
+			     G_SIGNAL_RUN_LAST | G_SIGNAL_DEPRECATED,
 			     G_STRUCT_OFFSET(HinawaSndUnitClass, disconnected),
 			     NULL, NULL,
 			     g_cclosure_marshal_VOID__VOID,
@@ -250,7 +262,9 @@ static void hinawa_snd_unit_init(HinawaSndUnit *self)
  * Instantiate [class@SndUnit] object and return the instance.
  *
  * Returns: an instance of [class@SndUnit].
+ *
  * Since: 1.3.
+ * Deprecated: 2.5. Use [method@Hitaki.SndUnit.new] instead.
  */
 HinawaSndUnit *hinawa_snd_unit_new(void)
 {
@@ -267,6 +281,8 @@ HinawaSndUnit *hinawa_snd_unit_new(void)
  * Open ALSA hwdep character device and check it for FireWire sound devices.
  *
  * Since: 0.4
+ * Deprecated: 2.5. Use implementation of [method@Hitaki.AlsaFirewire.open] in
+ *	       [class@Hitaki.SndUnit] instead.
  */
 void hinawa_snd_unit_open(HinawaSndUnit *self, gchar *path, GError **error)
 {
@@ -346,6 +362,8 @@ end:
  * Retrieve an instance of [class@FwNode] associated to the given unit.
  *
  * Since: 2.0.
+ * Deprecated: 2.5. Instantiate [class@FwNode] according to
+ *	       [property@Hitaki.AlsaFirewire:node-device] instead.
  */
 void hinawa_snd_unit_get_node(HinawaSndUnit *self, HinawaFwNode **node)
 {
@@ -367,6 +385,8 @@ void hinawa_snd_unit_get_node(HinawaSndUnit *self, HinawaFwNode **node)
  * Disallow corresponding ALSA driver to start packet streaming.
  *
  * Since: 0.3
+ * Deprecated: 2.5. Use implementation of [method@Hitaki.AlsaFirewire.lock] in
+ *	       [class@Hitaki.SndUnit] instead.
  */
 void hinawa_snd_unit_lock(HinawaSndUnit *self, GError **error)
 {
@@ -399,6 +419,8 @@ void hinawa_snd_unit_lock(HinawaSndUnit *self, GError **error)
  * Allow corresponding ALSA driver to start packet streaming.
  *
  * Since: 0.3
+ * Deprecated: 2.5. Use implementation of [method@Hitaki.AlsaFirewire.unlock] in
+ *	       [class@Hitaki.SndUnit] instead.
  */
 void hinawa_snd_unit_unlock(HinawaSndUnit *self, GError **error)
 {
@@ -598,6 +620,8 @@ static void finalize_src(GSource *gsrc)
  * device.
  *
  * Since: 1.4.
+ * Deprecated: 2.5. Use implementation of [method@Hitaki.AlsaFirewire.create_source] in
+ *	       [class@Hitaki.SndUnit] instead.
  */
 void hinawa_snd_unit_create_source(HinawaSndUnit *self, GSource **gsrc,
 				   GError **error)

doc/hinawa.toml.in
@@ -12,6 +12,10 @@ dependencies = [
   "GObject-2.0",
 ]
 
+related = [
+  "Hitaki-0.0",
+]
+
 [dependencies."GLib-2.0"]
 name = "GLib"
 description = "The base utility library"
@@ -21,6 +25,11 @@ docs_url = "https://docs.gtk.org/glib/"
 name = "GObject"
 description = "The base type system and object class"
 docs_url = "https://docs.gtk.org/gobject/"
+
+[related."Hitaki-0.0"]
+name = "Hitaki"
+description = "Library to abstract the access to the ALSA firewire hwdep device "
+docs_url = "https://alsa-project.github.io/libhitaki-doc/"
 
 [source-location]
 base_url = "https://github.com/alsa-project/libhinawa/blob/master/"

doc/urlmap.js
@@ -1,4 +1,5 @@
 baseURLs = [
     [ 'GLib', 'https://docs.gtk.org/glib/' ],
     [ 'GObject', 'https://docs.gtk.org/gobject/' ],
+    [ 'Hitaki', 'https://alsa-project.github.io/libhitaki-doc/' ],
 ]

src/hinawa_enum_types.h
@@ -125,6 +125,7 @@ typedef enum {
  * A representation of type for sound unit defined by Linux sound subsystem.
  *
  * Since: 1.0
+ * Deprecated: 2.5. Use [enum@Hitaki.AlsaFirewireType] in libhitaki library instead.
  */
 typedef enum {
 	HINAWA_SND_UNIT_TYPE_DICE = 1,
@@ -203,6 +204,7 @@ typedef enum {
  * A set of error code for [struct@GLib.Error] with domain which equals to Hinawa.SndUnitError.
  *
  * Since: 2.1
+ * Deprecated: 2.5. Use Hitaki.AlsaFirewireError in libhitaki library instead.
  */
 typedef enum {
 	HINAWA_SND_UNIT_ERROR_DISCONNECTED,
@@ -222,6 +224,7 @@ typedef enum {
  * A set of error code for [structGLib.Error] with domain which equals to Hinawa.SndDiceError.
  *
  * Since: 2.1
+ * Deprecated: 2.5. Use Hitaki.AlsaFirewireError in libhitaki library instead.
  */
 typedef enum {
 	HINAWA_SND_DICE_ERROR_TIMEOUT,
@@ -250,6 +253,7 @@ typedef enum {
  * A set of status code for Echo Audio Fireworks Transaction.
  *
  * Since: 2.1
+ * Deprecated: 2.5. Use Hitaki.EfwProtocolError in libhitaki library instead.
  */
 typedef enum {
 	HINAWA_SND_EFW_STATUS_OK		= 0,

src/snd_dice.c
@@ -9,6 +9,8 @@
  * A notification listener for Dice models.
  *
  * A [class@SndDice] listen to Dice notification and generates signal when received.
+ *
+ * Deprecated: 2.5. libhitaki library provides [class@Hitaki.SndDice] as the alternative.
  */
 
 /**
@@ -17,6 +19,7 @@
  * Return the [alias@GLib.Quark] for [struct@GLib.Error] which has code in Hinawa.SndDiceError.
  *
  * Since: 2.1
+ * Deprecated: 2.5.
  *
  * Returns: A [alias@GLib.Quark].
  */
@@ -74,11 +77,13 @@ static void hinawa_snd_dice_class_init(HinawaSndDiceClass *klass)
 	 * Emitted when Dice unit transfers notification.
 	 *
 	 * Since: 0.3
+	 * Deprecated: 2.5. Use implementation of [class@Hitaki.SndDice] for
+	 *	       [signal@Hitaki.QuadletNotification::notified] instead.
 	 */
 	dice_sigs[DICE_SIG_TYPE_NOTIFIED] =
 		g_signal_new("notified",
 			     G_OBJECT_CLASS_TYPE(klass),
-			     G_SIGNAL_RUN_LAST,
+			     G_SIGNAL_RUN_LAST | G_SIGNAL_DEPRECATED,
 			     G_STRUCT_OFFSET(HinawaSndDiceClass, notified),
 			     NULL, NULL,
 			     g_cclosure_marshal_VOID__UINT,
@@ -96,7 +101,9 @@ static void hinawa_snd_dice_init(HinawaSndDice *self)
  * Instantiate [class@SndDice] object and return the instance.
  *
  * Returns: an instance of [class@SndDice].
+ *
  * Since: 1.3.
+ * Deprecated: 2.5. Use [method@Hitaki.SndDice.new] instead.
  */
 HinawaSndDice *hinawa_snd_dice_new(void)
 {
@@ -113,6 +120,8 @@ HinawaSndDice *hinawa_snd_dice_new(void)
  * Open ALSA hwdep character device and check it for Dice devices.
  *
  * Since: 0.4
+ * Deprecated: 2.5. Use implementation of [method@Hitaki.AlsaFirewire.open] in
+ *	       [class@Hitaki.SndDice] instead.
  */
 void hinawa_snd_dice_open(HinawaSndDice *self, gchar *path, GError **error)
 {
@@ -147,6 +156,9 @@ void hinawa_snd_dice_open(HinawaSndDice *self, gchar *path, GError **error)
  * Execute write transactions to the given address, then wait and check notification.
  *
  * Since: 1.4.
+ * Deprecated: 2.5. Use [class@FwReq] to send write request transaction to the unit, then use
+ *		    implementaion of [signal@Hitaki.QuadletNotification::notified] in
+ *		    [class@Hitaki.SndDice] to wait for notification.
  */
 void hinawa_snd_dice_transaction(HinawaSndDice *self, guint64 addr,
 			         const guint32 *frame, gsize frame_count,

src/snd_dice.h
@@ -25,6 +25,8 @@ struct _HinawaSndDiceClass {
 	 * Class closure for the [signal@SndDice::notified].
 	 *
 	 * Since: 1.2
+	 * Deprecated: 2.5. Use implementation of [class@Hitaki.SndDice] for
+	 *	       [vfunc@Hitaki.QuadletNotification.notified] instead.
 	 */
 	void (*notified)(HinawaSndDice *self, guint message);
 };

commit 3a69bf3ecea6ed82cc955df1249fdef0035cc1a4

snd_unit/snd_dg00x: fix annotation to Hitaki-0.0 namespace
Fixes: 12881a0b1af3 ("snd_*: add deprecation note and flag to delegate to libhitaki")
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/snd_dg00x.c
@@ -32,7 +32,7 @@ static void hinawa_snd_dg00x_class_init(HinawaSndDg00xClass *klass)
 	 * Emitted when Dg00x models transfer notification.
 	 *
 	 * Since: 0.7
-	 * Deprecated: 2.5. Use implementation of [signal@Hitaki.AlsaFirewire::notified] in
+	 * Deprecated: 2.5. Use implementation of [signal@Hitaki.QuadletNotification::notified] in
 	 *	       [class@Hitaki.SndDigi00x] instead.
 	 */
 	dg00x_sigs[DG00X_SIG_TYPE_MESSAGE] =

src/snd_dg00x.h
@@ -21,7 +21,7 @@ struct _HinawaSndDg00xClass {
 	 * Class closure for the [signal@SndDg00x::message] signal.
 	 *
 	 * Since: 1.2
-	 * Deprecated: 2.5. Use implementation of [vfunc@Hitaki.AlsaFirewire.notified] in
+	 * Deprecated: 2.5. Use implementation of [vfunc@Hitaki.QuadletNotification.notified] in
 	 *	       [class@Hitaki.SndDigi00x] instead.
 	 */
 	void (*message)(HinawaSndDg00x *self, guint32 message);

src/snd_unit.c
@@ -155,7 +155,7 @@ static void hinawa_snd_unit_class_init(HinawaSndUnitClass *klass)
 	 * HinawaSndUnit:card:
 	 *
 	 * Since: 2.0
-	 * Deprecated: 2.5. Use [property@Hitaki.AlsaFirewire:card] instead.
+	 * Deprecated: 2.5. Use [property@Hitaki.AlsaFirewire:card_id] instead.
 	 */
 	snd_unit_props[SND_UNIT_PROP_TYPE_CARD_ID] =
 		g_param_spec_uint("card", "card",