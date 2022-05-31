alsa-project/libhinawa

commit c237d032e91a94d4b01fc94a0dfdea77c4a66c01

add entry header to aggregate headers
It's convenient to add header to include all of headers in regard of
documentation.

Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/hinawa.h
@@ -1,0 +1,31 @@
+// SPDX-License-Identifier: LGPL-2.1-or-later
+#ifndef __ALSA_HINAWA_H__
+#define __ALSA_HINAWA_H__
+
+#include <glib.h>
+#include <glib-object.h>
+
+#include <linux/firewire-cdev.h>
+#include <linux/firewire-constants.h>
+#include <sound/firewire.h>
+
+#include <hinawa_sigs_marshal.h>
+
+#include <hinawa_enum_types.h>
+#include <hinawa_enums.h>
+
+#include <snd_motu_register_dsp_parameter.h>
+
+#include <fw_node.h>
+#include <fw_resp.h>
+#include <fw_req.h>
+#include <fw_fcp.h>
+
+#include <snd_unit.h>
+#include <snd_dice.h>
+#include <snd_efw.h>
+#include <snd_dg00x.h>
+#include <snd_motu.h>
+#include <snd_tscm.h>
+
+#endif

src/hinawa_enum_types.h
@@ -2,10 +2,6 @@
 #ifndef __ALSA_HINAWA_ENUM_TYPES_H__
 #define __ALSA_HINAWA_ENUM_TYPES_H__
 
-#include <glib-object.h>
-#include <linux/firewire-constants.h>
-#include <sound/firewire.h>
-
 /**
  * HinawaFwTcode:
  * @HINAWA_FW_TCODE_WRITE_QUADLET_REQUEST:	For request to write quadlet.

src/fw_fcp.c
@@ -1,10 +1,8 @@
 // SPDX-License-Identifier: LGPL-2.1-or-later
+#include "internal.h"
+
 #include <string.h>
 #include <errno.h>
-
-#include "internal.h"
-#include "fw_fcp.h"
-#include "hinawa_sigs_marshal.h"
 
 /**
  * SECTION:fw_fcp

src/fw_fcp.h
@@ -2,12 +2,7 @@
 #ifndef __ALSA_HINAWA_FW_FCP_H__
 #define __ALSA_HINAWA_FW_FCP_H__
 
-#include <glib.h>
-#include <glib-object.h>
-
-#include <fw_node.h>
-#include <fw_resp.h>
-#include <hinawa_enums.h>
+#include <hinawa.h>
 
 G_BEGIN_DECLS
 

src/fw_node.c
@@ -8,8 +8,6 @@
 #include <sys/ioctl.h>
 #include <errno.h>
 
-#include <linux/firewire-cdev.h>
-
 /**
  * SECTION:fw_node
  * @Title: HinawaFwNode

src/fw_node.h
@@ -2,8 +2,7 @@
 #ifndef __ALSA_HINAWA_FW_NODE_H__
 #define __ALSA_HINAWA_FW_NODE_H__
 
-#include <glib.h>
-#include <glib-object.h>
+#include <hinawa.h>
 
 G_BEGIN_DECLS
 

src/fw_req.c
@@ -1,10 +1,9 @@
 // SPDX-License-Identifier: LGPL-2.1-or-later
+#include "internal.h"
+
 #include <string.h>
 #include <errno.h>
 #include <sys/ioctl.h>
-
-#include "internal.h"
-#include "hinawa_sigs_marshal.h"
 
 /**
  * SECTION:fw_req

src/fw_req.h
@@ -2,11 +2,7 @@
 #ifndef __ALSA_HINAWA_FW_REQ_H__
 #define __ALSA_HINAWA_FW_REQ_H__
 
-#include <glib.h>
-#include <glib-object.h>
-
-#include <fw_node.h>
-#include <hinawa_enums.h>
+#include <hinawa.h>
 
 G_BEGIN_DECLS
 

commit 49194fe9cd155416046a56b5d2d379501972d8c0

fix missing usage of declaration macro
Fixes: c237d032e91a ("add entry header to aggregate headers")
Fixes: 91717078c1c2 ("add support subclasses of GObject enumeration class")
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/hinawa.h
@@ -2,6 +2,8 @@
 #ifndef __ALSA_HINAWA_H__
 #define __ALSA_HINAWA_H__
 
+G_BEGIN_DECLS
+
 #include <glib.h>
 #include <glib-object.h>
 
@@ -27,5 +29,7 @@
 #include <snd_dg00x.h>
 #include <snd_motu.h>
 #include <snd_tscm.h>
+
+G_END_DECLS
 
 #endif

src/hinawa_enum_types.h
@@ -2,6 +2,8 @@
 #ifndef __ALSA_HINAWA_ENUM_TYPES_H__
 #define __ALSA_HINAWA_ENUM_TYPES_H__
 
+G_BEGIN_DECLS
+
 /**
  * HinawaFwTcode:
  * @HINAWA_FW_TCODE_WRITE_QUADLET_REQUEST:	For request to write quadlet.
@@ -274,5 +276,7 @@ typedef enum {
 	HINAWA_SND_EFW_STATUS_BAD_PARAMETER	= 15,
 	HINAWA_SND_EFW_STATUS_LARGE_RESP,
 } HinawaSndEfwStatus;
+
+G_END_DECLS
 
 #endif