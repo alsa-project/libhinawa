07b46b232f78fd9e659d76a11276d9e15e66460b
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2022-06-17 17:37:29 +0900

    tests: test object type instead of its instance
    No need to instantiate object to test.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- tests/helper.py
+++ tests/helper.py
@@ -2,21 +2,21 @@ import gi
 gi.require_version('GObject', '2.0')
 from gi.repository import GObject
 
-def test_object(target, props, methods, vmethods, signals) ->bool:
-    labels = [prop.name for prop in target.props]
+def test_object(target_type, props, methods, vmethods, signals) ->bool:
+    labels = [prop.name for prop in target_type.props]
     for prop in props:
         if prop not in labels:
             print('Property {0} is not produced.'.format(prop))
             return False
     for method in methods:
-        if not hasattr(target, method):
+        if not hasattr(target_type, method):
             print('Method {0} is not produced.'.format(method))
             return False
     for vmethod in vmethods:
-        if not hasattr(target, method):
+        if not hasattr(target_type, method):
             print('Vmethod {0} is not produced.'.format(vmethod))
             return False
-    labels = GObject.signal_list_names(target)
+    labels = GObject.signal_list_names(target_type)
     for signal in signals:
         if signal not in labels:
             print('Signal {0} is not produced.'.format(signal))
