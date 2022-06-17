alsa-project/libhinawa

commit 49194fe9cd155416046a56b5d2d379501972d8c0

fix missing usage of declaration macro
Fixes: c237d032e91a ("add entry header to aggregate headers")
Fixes: 91717078c1c2 ("add support subclasses of GObject enumeration class")
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/hinawa.h
@@ -2,6 +2,8 @@
 #ifndef __ALSA_HINAWA_H__
 #define __ALSA_HINAWA_H__
 
+G_BEGIN_DECLS
+
 #include <glib.h>
 #include <glib-object.h>
 
@@ -27,5 +29,7 @@
 #include <snd_dg00x.h>
 #include <snd_motu.h>
 #include <snd_tscm.h>
+
+G_END_DECLS
 
 #endif

src/hinawa_enum_types.h
@@ -2,6 +2,8 @@
 #ifndef __ALSA_HINAWA_ENUM_TYPES_H__
 #define __ALSA_HINAWA_ENUM_TYPES_H__
 
+G_BEGIN_DECLS
+
 /**
  * HinawaFwTcode:
  * @HINAWA_FW_TCODE_WRITE_QUADLET_REQUEST:	For request to write quadlet.
@@ -274,5 +276,7 @@ typedef enum {
 	HINAWA_SND_EFW_STATUS_BAD_PARAMETER	= 15,
 	HINAWA_SND_EFW_STATUS_LARGE_RESP,
 } HinawaSndEfwStatus;
+
+G_END_DECLS
 
 #endif

commit 55e0156bc751a93fc6b475d4abb6c4a9a460ce36

fix FTBFS due to superfluous usage of macro
Fixes: 49194fe9cd15 ("fix missing usage of declaration macro")
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

src/hinawa.h
@@ -2,8 +2,6 @@
 #ifndef __ALSA_HINAWA_H__
 #define __ALSA_HINAWA_H__
 
-G_BEGIN_DECLS
-
 #include <glib.h>
 #include <glib-object.h>
 
@@ -29,7 +27,5 @@ G_BEGIN_DECLS
 #include <snd_dg00x.h>
 #include <snd_motu.h>
 #include <snd_tscm.h>
-
-G_END_DECLS
 
 #endif