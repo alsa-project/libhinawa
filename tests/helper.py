b3a7b3845a5efa061f3b75c7362c91b254249b4e
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2022-06-17 17:37:29 +0900

    tests: refine helper function to test object
    When implementing GObject interface, available properties, virtual
    methods, and signals are not referred by attributes of object, while
    available methods are available.
    
    This commit expands current helper function to test object interface.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- tests/helper.py
+++ tests/helper.py
@@ -1,24 +1,37 @@
-import gi
-gi.require_version('GObject', '2.0')
-from gi.repository import GObject
-
-def test_object(target_type, props, methods, vmethods, signals) ->bool:
-    labels = [prop.name for prop in target_type.props]
-    for prop in props:
-        if prop not in labels:
-            print('Property {0} is not produced.'.format(prop))
-            return False
+def test_object(target_type: object, props: tuple[str], methods: tuple[str],
+                vmethods: tuple[str], signals: tuple[str]) -> bool:
+    # All of available methods are put into the list of attribute.
     for method in methods:
         if not hasattr(target_type, method):
             print('Method {0} is not produced.'.format(method))
             return False
+
+    # The properties, virtual methods, and signals in interface are not put
+    # into the list of attribute in object implementing the interface.
+    prop_labels = []
+    vmethod_labels = []
+    signal_labels = []
+
+    # The  gi.ObjectInfo and gi.InterfaceInfo keeps them. Let's traverse them.
+    for info in target_type.__mro__:
+        if hasattr(info, '__info__'):
+            for prop in info.__info__.get_properties():
+                prop_labels.append(prop.get_name())
+            for vfunc in info.__info__.get_vfuncs():
+                vmethod_labels.append('do_' + vfunc.get_name())
+            for signal in info.__info__.get_signals():
+                signal_labels.append(signal.get_name())
+
+    for prop in props:
+        if prop not in prop_labels:
+            print('Property {0} is not produced.'.format(prop))
+            return False
     for vmethod in vmethods:
-        if not hasattr(target_type, method):
+        if vmethod not in vmethod_labels:
             print('Vmethod {0} is not produced.'.format(vmethod))
             return False
-    labels = GObject.signal_list_names(target_type)
     for signal in signals:
-        if signal not in labels:
+        if signal not in signal_labels:
             print('Signal {0} is not produced.'.format(signal))
             return False
     return True
