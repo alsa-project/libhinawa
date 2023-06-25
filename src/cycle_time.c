333a565d315ba7d9f8ce821ffc23fcb19678d75d
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2023-06-25 22:04:35 +0900

    cycle_time: add methods to compute isochronous cycle from time stamp
    When comparing to isochronous cycle calculated from the value of
    CYCLE_TIME register, the second field in time stamp retrieved from
    each context of 1394 OHCI should be completed, since it has just
    three bits.
    
    This commit adds new method to compute the isochronous cycle from the
    time stamp as well as new method to parse the time stamp as is.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- src/cycle_time.c
+++ src/cycle_time.c
@@ -123,3 +123,63 @@ void hinawa_cycle_time_get_raw(HinawaCycleTime *self, guint32 *raw)
 {
 	*raw = self->cycle_timer;
 }
+
+#define OHCI1394_TSTAMP_SEC_MASK	0x0000e000
+#define OHCI1394_TSTAMP_SEC_SHIFT	13
+#define OHCI1394_TSTAMP_CYCLE_MASK	0x00001fff
+#define OHCI1394_TSTAMP_CYCLE_SHIFT	0
+
+#define IEEE1394_SEC_MAX	128
+#define OHCI1394_SEC_MAX	8
+
+/**
+ * hinawa_cycle_time_compute_tstamp:
+ * @self: A [struct@CycleTime].
+ * @tstamp: The value of time stamp retrieved from each context of 1394 OHCI.
+ * @isoc_cycle: (array fixed-size=2) (inout): The result to parse the time stamp. The first element
+ *	    is for 7 bits of second field in the format of IEEE 1394 CYCLE_TIME register, up to
+ *	    127. The second element is for 13 bits of cycle field in the format, up to 7,999.
+ *
+ * Compute second count and cycle count from unsigned 16 bit integer value retrieved by Asynchronous
+ * Transmit (AT), Asynchronous Receive(AR), Isochronous Transmit (IT), and Isochronous Receive (IR)
+ * contexts of 1394 OHCI. The second count is completed with the internal value read from the
+ * CYCLE_TIME register. For the precise computation, the method should be called in the condition
+ * that the timing between receipt of time stamp and access to CYCLE_TIME register is within 8
+ * seconds.
+ *
+ * Since: 2.6
+ */
+void hinawa_cycle_time_compute_tstamp(HinawaCycleTime *self, guint tstamp, guint **isoc_cycle)
+{
+	guint tstamp_sec_low = (tstamp & OHCI1394_TSTAMP_SEC_MASK) >> OHCI1394_TSTAMP_SEC_SHIFT;
+	guint curr_sec_low = ieee1394_cycle_time_to_sec(self->cycle_timer) & 0x7;
+	guint sec = ieee1394_cycle_time_to_sec(self->cycle_timer);
+
+	// Round up.
+	if (tstamp_sec_low < curr_sec_low)
+		sec += OHCI1394_SEC_MAX;
+	sec |= tstamp_sec_low;
+	sec %= IEEE1394_SEC_MAX;
+
+	(*isoc_cycle)[0] = sec;
+	(*isoc_cycle)[1] = (tstamp & OHCI1394_TSTAMP_CYCLE_MASK) >> OHCI1394_TSTAMP_CYCLE_SHIFT;
+}
+
+/**
+ * hinawa_cycle_time_parse_tstamp:
+ * @tstamp: The value of time stamp retrieved from each context of 1394 OHCI.
+ * @isoc_cycle: (array fixed-size=2) (inout): The result to parse the time stamp. The first element
+ *	    is for three order bits of second field in the format of IEEE 1394 CYCLE_TIME register,
+ *	    up to 7. The second element is for 13 bits of cycle field in the format, up to 7,999.
+ *
+ * Parse second count and cycle count from unsigned 16 bit integer value retrieved by Asynchronous
+ * Transmit (AT), Asynchronous Receive(AR), Isochronous Transmit (IT), and Isochronous Receive (IR)
+ * contexts of 1394 OHCI.
+ *
+ * Since: 2.6
+ */
+void hinawa_cycle_time_parse_tstamp(guint tstamp, guint **isoc_cycle)
+{
+	(*isoc_cycle)[0] = (tstamp & OHCI1394_TSTAMP_SEC_MASK) >> OHCI1394_TSTAMP_SEC_SHIFT;
+	(*isoc_cycle)[1] = (tstamp & OHCI1394_TSTAMP_CYCLE_MASK) >> OHCI1394_TSTAMP_CYCLE_SHIFT;
+}
