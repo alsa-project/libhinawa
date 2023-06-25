adbedb7ab7df4daf92922cf419eba3a4d79b2185
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2023-06-25 22:04:35 +0900

    fw_node: add a new method to read CYCLE_TIMER register in 1394 OHCI controller
    This commit adds a new method to read data for CYCLE_TIMER register in
    1394 OHCI controller.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- src/fw_node.h
+++ src/fw_node.h
@@ -47,6 +47,9 @@ void hinawa_fw_node_open(HinawaFwNode *self, const gchar *path,
 void hinawa_fw_node_get_config_rom(HinawaFwNode *self, const guint8 **image,
 				   gsize *length, GError **error);
 
+gboolean hinawa_fw_node_read_cycle_time(HinawaFwNode *self, gint clock_id,
+					HinawaCycleTime *const *cycle_time, GError **error);
+
 void hinawa_fw_node_create_source(HinawaFwNode *self, GSource **gsrc,
 				  GError **error);
 
