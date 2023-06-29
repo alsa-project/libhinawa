ee23d4fe86527dc4a50ddcdb742ae72f9aebd456
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2023-06-29 12:52:17 +0900

    samples: use new methods for asynchronous transaction with time stamps
    This commit uses new methods for asynchronous transaction with time
    stamps in sample scripts.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

--- samples/common/__init__.py
+++ samples/common/__init__.py
@@ -8,6 +8,8 @@ import gi
 gi.require_versions({'GLib': '2.0', 'Hinawa': '3.0'})
 from gi.repository import GLib, Hinawa
 
+CLOCK_MONOTONIC_RAW = 4
+
 
 def print_help_with_msg(cmd: str, msg: str):
     print('Error:', file=stderr)
@@ -61,6 +63,53 @@ def print_fw_node_information(node: Hinawa.FwNode):
         print('    0xfffff00004{:02x}: 0x{:08x}'.format(i * 4, q))
 
 
+def read_quadlet(node: Hinawa.FwNode, req: Hinawa.FwReq, addr: int) -> int:
+    cycle_time = Hinawa.CycleTime.new()
+
+    try:
+        _, cycle_time = node.read_cycle_time(CLOCK_MONOTONIC_RAW, cycle_time)
+    except Exception as e:
+        print(e)
+        return 0
+    initiate_cycle = cycle_time.get_fields()
+
+    frame = [0] * 4
+    tstamp = [0] * 2
+    try:
+        _, frame, tstamp = req.transaction_with_tstamp_sync(
+            node,
+            Hinawa.FwTcode.READ_QUADLET_REQUEST,
+            addr,
+            len(frame),
+            frame,
+            tstamp,
+            100
+        )
+    except Exception as e:
+        print(e)
+
+    sent_cycle = cycle_time.compute_tstamp(tstamp[0], [0] * 2)
+    recv_cycle = cycle_time.compute_tstamp(tstamp[1], [0] * 2)
+
+    try:
+        _, cycle_time = node.read_cycle_time(CLOCK_MONOTONIC_RAW, cycle_time)
+    except Exception as e:
+        print(e)
+        return 0
+    finish_cycle = cycle_time.get_fields()
+
+    quadlet = unpack('>I', frame)[0]
+
+    print('Read quadlet transaction:')
+    print('  addr 0x{:012x}, quadlet: 0x{:08x}'.format(addr, quadlet))
+    print('  initiate at:  sec {} cycle {}'.format(initiate_cycle[0], initiate_cycle[1]))
+    print('  sent at:      sec {} cycle {}'.format(sent_cycle[0], sent_cycle[1]))
+    print('  received at:  sec {} cycle {}'.format(recv_cycle[0], recv_cycle[1]))
+    print('  finish at:    sec {} cycle {}'.format(finish_cycle[0], finish_cycle[1]))
+
+    return quadlet
+
+
 @contextmanager
 def run_dispatcher(node: Hinawa.FwNode):
     ctx = GLib.MainContext.new()
@@ -94,18 +143,28 @@ def print_frame(frame: list):
         print('  [{:02d}]: 0x{:02x}'.format(i, frame[i]))
 
 
-def handle_requested2(resp: Hinawa.FwResp, tcode: Hinawa.FwRcode, offset: int,
-                      src: int, dst: int, card: int, generation: int, frame: list,
-                      length: int):
-    print('Event requested2: {0}'.format(tcode.value_nick))
+def handle_requested3(resp: Hinawa.FwResp, tcode: Hinawa.FwRcode, offset: int,
+                      src: int, dst: int, card: int, generation: int, tstamp: int,
+                      frame: list, length: int, args: tuple[Hinawa.FwNode, Hinawa.CycleTime]):
+    node, cycle_time = args
+    print('Event requested3: {0}'.format(tcode.value_nick))
+    try:
+        _, cycle_time = node.read_cycle_time(CLOCK_MONOTONIC_RAW, cycle_time)
+        isoc_cycle = cycle_time.compute_tstamp(tstamp, [0] * 2)
+    except Exception as e:
+        print(e)
+        isoc_cycle = [0] * 2
+        pass
     print_frame(frame)
+    print('  received at: sec {0[0]} cycle {0[1]}'.format(isoc_cycle))
     return Hinawa.FwRcode.COMPLETE
 
 
 @contextmanager
 def listen_region(node: Hinawa.FwNode):
     resp = Hinawa.FwResp()
-    handler = resp.connect('requested2', handle_requested2)
+    cycle_time = Hinawa.CycleTime.new()
+    handler = resp.connect('requested3', handle_requested3, (node, cycle_time))
     try:
         resp.reserve(node, 0xfffff0000d00, 0x100)
         yield
@@ -116,25 +175,55 @@ def listen_region(node: Hinawa.FwNode):
     resp.release()
 
 
-def handle_responded(fcp: Hinawa.FwFcp, frame: list, length: int):
-    print('Event responded: length {}'.format(length))
+def handle_responded2(fcp: Hinawa.FwFcp, frame: list, length: int, tstamp: int,
+                      args: tuple[Hinawa.FwNode, Hinawa.CycleTime]):
+    node, cycle_time = args
+    print('Event responded2: length {}'.format(length))
+    try:
+        _, cycle_time = node.read_cycle_time(CLOCK_MONOTONIC_RAW, cycle_time)
+        isoc_cycle = cycle_time.compute_tstamp(tstamp, [0] * 2)
+    except Exception as e:
+        print(e)
+        isoc_cycle = [0] * 2
+        pass
     print_frame(frame)
+    print('  received at: sec {0[0]} cycle {0[1]}'.format(isoc_cycle))
 
 
 @contextmanager
 def listen_fcp(node: Hinawa.FwNode):
+    cycle_time = Hinawa.CycleTime.new()
+
     fcp = Hinawa.FwFcp()
-    handler = fcp.connect('responded', handle_responded)
+    handler = fcp.connect('responded2', handle_responded2, (node, cycle_time))
     try:
         fcp.bind(node)
 
+        _, cycle_time = node.read_cycle_time(CLOCK_MONOTONIC_RAW, cycle_time)
+        initiate_cycle = cycle_time.get_fields()
+
+        tstamp = [0] * 3
         request = bytes([0x01, 0xff, 0x19, 0x00, 0xff, 0xff, 0xff, 0xff])
+        _, response, tstamp = fcp.avc_transaction_with_tstamp(request, [0] * len(request), tstamp,
+                                                              100)
+
+        req_sent_cycle = cycle_time.compute_tstamp(tstamp[0], [0] * 2)
+        req_responded_cycle = cycle_time.compute_tstamp(tstamp[1], [0] * 2)
+        resp_sent_cycle = cycle_time.compute_tstamp(tstamp[2], [0] * 2)
+
+        _, cycle_time = node.read_cycle_time(CLOCK_MONOTONIC_RAW, cycle_time)
+        finish_cycle = cycle_time.get_fields()
+
         print('FCP request:')
         print_frame(request)
+        print('  initiate at:  sec {0[0]} cycle {0[1]}'.format(initiate_cycle))
+        print('  sent at:      sec {0[0]} cycle {0[1]}'.format(req_sent_cycle))
+        print('  received at:  sec {0[0]} cycle {0[1]}'.format(req_responded_cycle))
 
-        response = fcp.avc_transaction(request, [0] * len(request), 100)
         print('FCP response:')
         print_frame(response)
+        print('  received at:  sec {0[0]} cycle {0[1]}'.format(resp_sent_cycle))
+        print('  finished at:  sec {0[0]} cycle {0[1]}'.format(finish_cycle))
 
         yield
     except Exception as e:
@@ -165,4 +254,5 @@ def listen_node_event(node: Hinawa.FwNode, path: Path):
             yield
 
 
-__all__ = ['print_help_with_msg', 'detect_fw_cdev', 'dump_fw_node_information', 'listen_node_event']
+__all__ = ['print_help_with_msg', 'detect_fw_cdev', 'run_async_transaction',
+           'dump_fw_node_information', 'listen_node_event']
